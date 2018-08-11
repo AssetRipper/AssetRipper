e6208690467912826b934e6729349ee6422809b6
Xele02 <git@xele.org>
2018-08-11 12:33:47 +0200

    Fix scripts properties array

---
+++ UtinyRipperCore/Parser/AssetCollection/Exporter/Exporters/Script/Elements/Mono/ScriptExportMonoArray.cs
@@ -0,0 +1,88 @@
+﻿using Mono.Cecil;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UtinyRipper.Exporters.Scripts.Mono
+{
+	public sealed class ScriptExportMonoArray : ScriptExportArray
+	{
+		public ScriptExportMonoArray(TypeReference @type, TypeDefinition @definition)
+		{
+			if (@type == null || @definition == null)
+			{
+				throw new ArgumentNullException(nameof(@type));
+			}
+			if (!type.IsArray)
+			{
+				throw new Exception("Type isn't an array");
+			}
+
+			Type = @type;
+			Definition = @definition;
+		}
+
+		public override void Init(IScriptExportManager manager)
+		{
+			m_element = CreateElementType(manager);
+
+			m_module = Path.GetFileNameWithoutExtension(Type.Scope.Name);
+			m_fullName = $"[{Module}]{Type.FullName}";
+		}
+
+
+		public override ScriptExportType GetContainer(IScriptExportManager manager)
+		{
+			if (Type.IsNested)
+			{
+				return manager.RetrieveType(Type.DeclaringType);
+			}
+			else
+			{
+				return this;
+			}
+		}
+
+
+		private ScriptExportType CreateElementType(IScriptExportManager manager)
+		{
+			return manager.RetrieveType(Type.GetElementType());
+		}
+
+
+		protected override string Keyword
+		{
+			get
+			{
+				if (Definition.IsPublic || Definition.IsNestedPublic)
+				{
+					return PublicKeyWord;
+				}
+				if (Definition.IsNestedPrivate)
+				{
+					return PrivateKeyWord;
+				}
+				if (Definition.IsNestedFamily)
+				{
+					return ProtectedKeyWord;
+				}
+				return InternalKeyWord;
+			}
+		}
+
+
+		public override string FullName => m_fullName;
+		public override string Name => Type.Name;
+		public override string Namespace => Type.Namespace;
+		public override string Module => m_module;
+
+		protected override ScriptExportType Element => m_element;
+
+		private TypeReference Type { get; }
+		private TypeDefinition Definition { get; }
+
+		private ScriptExportType m_element;
+		private string m_fullName;
+		private string m_module;
+	}
+}
---
+++ UtinyRipperCore/Parser/AssetCollection/Exporter/Exporters/Script/Elements/ScriptExportArray.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace UtinyRipper.Exporters.Scripts
+{
+	public abstract class ScriptExportArray : ScriptExportType
+	{      
+		public override void GetUsedNamespaces(ICollection<string> namespaces)
+		{
+			GetTypeNamespaces(namespaces);
+			Element.GetTypeNamespaces(namespaces);
+		}
+
+		protected override ScriptExportType Base => null;
+
+		public override IReadOnlyList<ScriptExportType> GenericArguments { get; } = new ScriptExportType[0];
+		public override IReadOnlyList<ScriptExportType> NestedTypes { get; } = new ScriptExportType[0];
+		public override IReadOnlyList<ScriptExportEnum> NestedEnums { get; } = new ScriptExportEnum[0];
+		public override IReadOnlyList<ScriptExportDelegate> Delegates { get; } = new ScriptExportDelegate[0];
+		public override IReadOnlyList<ScriptExportField> Fields { get; } = new ScriptExportField[0];
+
+		protected abstract ScriptExportType Element { get; }
+
+		protected override bool IsStruct => false;
+		protected override bool IsSerializable => false;
+
+	}
+}
