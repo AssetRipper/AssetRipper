b269152f7aacfbc3284ab5165f73e378203ffc41
mafaca <mafaca@qq.com>
2018-10-12 16:02:01 +0400

    implemented missed RectTransform export

--- uTinyRipperCore/Parser/Classes/RectTransform.cs
+++ uTinyRipperCore/Parser/Classes/RectTransform.cs
@@ -1,4 +1,7 @@
-﻿namespace uTinyRipper.Classes
+﻿using uTinyRipper.AssetExporters;
+using uTinyRipper.Exporter.YAML;
+
+namespace uTinyRipper.Classes
 {
 	public sealed class RectTransform : Transform
 	{
@@ -16,6 +19,17 @@
 			AnchorPosition.Read(reader);
 			SizeDelta.Read(reader);
 			Pivot.Read(reader);
+		}
+
+		protected override YAMLMappingNode ExportYAMLRoot(IExportContainer container)
+		{
+			YAMLMappingNode node = base.ExportYAMLRoot(container);
+			node.Add("m_AnchorMin", AnchorMin.ExportYAML(container));
+			node.Add("m_AnchorMax", AnchorMax.ExportYAML(container));
+			node.Add("m_AnchoredPosition", AnchorPosition.ExportYAML(container));
+			node.Add("m_SizeDelta", SizeDelta.ExportYAML(container));
+			node.Add("m_Pivot", Pivot.ExportYAML(container));
+			return node;
 		}
 
 		public Vector2f AnchorMin;
