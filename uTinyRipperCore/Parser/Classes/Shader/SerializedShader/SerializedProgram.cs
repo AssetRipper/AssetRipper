17f9fcb9229d9032571b9b6eccd5665af8c6b241
mafaca <mafaca@qq.com>
2019-03-18 21:51:02 +0300

    corrected coding standard

--- uTinyRipperCore/Parser/Classes/Shader/SerializedShader/SerializedProgram.cs
+++ uTinyRipperCore/Parser/Classes/Shader/SerializedShader/SerializedProgram.cs
@@ -1,61 +1,58 @@
 using System.Collections.Generic;
-using System.Linq;
-using System.IO;
-using System;
-using uTinyRipper.Classes.Shaders.Exporters;
 
 namespace uTinyRipper.Classes.Shaders
 {
-    public struct SerializedProgram : IAssetReadable
-    {
-        public void Read(AssetReader reader)
-        {
-          m_subPrograms = reader.ReadAssetArray<SerializedSubProgram>();
-        }
-        Dictionary<ShaderGpuProgramType, int> GetIsTierLookup(IReadOnlyList<SerializedSubProgram> subPrograms)
-        {
-            var lookup = new Dictionary<ShaderGpuProgramType, int>();
-            var seen = new Dictionary<ShaderGpuProgramType, byte>();
-            foreach (var subProgram in subPrograms)
-            {
-                if (seen.ContainsKey(subProgram.GpuProgramType))
-                {
-                    if (seen[subProgram.GpuProgramType] != subProgram.ShaderHardwareTier)
-                    {
-                        lookup[subProgram.GpuProgramType] = 2;
-                    }
-                }
-                else
-                {
-                    seen[subProgram.GpuProgramType] = subProgram.ShaderHardwareTier;
-                    lookup[subProgram.GpuProgramType] = 1;
-                }
-            }
-            return lookup;
-        }
-        public void Export(ShaderWriter writer, ShaderType type)
-        {
-            if(SubPrograms.Count > 0)
-            {
-                writer.WriteIntent(3);
-                writer.Write("Program \"{0}\" {{\n", type.ToProgramTypeString());
-                var isTierLookup = GetIsTierLookup(SubPrograms);
-                foreach (SerializedSubProgram subProgram in SubPrograms)
-                {
-                    Platform uplatform = writer.Platform;
-                    GPUPlatform platform = subProgram.GpuProgramType.ToGPUPlatform(uplatform);
-                    int index = writer.Shader.Platforms.IndexOf(platform);
-                    ShaderSubProgramBlob blob = writer.Shader.SubProgramBlobs[index];
-                    int count = isTierLookup[subProgram.GpuProgramType];
-                    subProgram.Export(writer, blob, count > 1);
-                }
-                writer.WriteIntent(3);
-                writer.Write("}\n");
-            }
-        }
+	public struct SerializedProgram : IAssetReadable
+	{
+		public void Read(AssetReader reader)
+		{
+			m_subPrograms = reader.ReadAssetArray<SerializedSubProgram>();
+		}
 
-        public IReadOnlyList<SerializedSubProgram> SubPrograms => m_subPrograms;
+		public void Export(ShaderWriter writer, ShaderType type)
+		{
+			if (SubPrograms.Count > 0)
+			{
+				writer.WriteIntent(3);
+				writer.Write("Program \"{0}\" {{\n", type.ToProgramTypeString());
+				HashSet<ShaderGpuProgramType> isTierLookup = GetIsTierLookup(SubPrograms);
+				foreach (SerializedSubProgram subProgram in SubPrograms)
+				{
+					Platform uplatform = writer.Platform;
+					GPUPlatform platform = subProgram.GpuProgramType.ToGPUPlatform(uplatform);
+					int index = writer.Shader.Platforms.IndexOf(platform);
+					ShaderSubProgramBlob blob = writer.Shader.SubProgramBlobs[index];
+					bool isTier = isTierLookup.Contains(subProgram.GpuProgramType);
+					subProgram.Export(writer, blob, isTier);
+				}
+				writer.WriteIntent(3);
+				writer.Write("}\n");
+			}
+		}
 
-        private SerializedSubProgram[] m_subPrograms;
-    }
+		private HashSet<ShaderGpuProgramType> GetIsTierLookup(IReadOnlyList<SerializedSubProgram> subPrograms)
+		{
+			HashSet<ShaderGpuProgramType> lookup = new HashSet<ShaderGpuProgramType>();
+			Dictionary<ShaderGpuProgramType, byte> seen = new Dictionary<ShaderGpuProgramType, byte>();
+			foreach (SerializedSubProgram subProgram in subPrograms)
+			{
+				if (seen.ContainsKey(subProgram.GpuProgramType))
+				{
+					if (seen[subProgram.GpuProgramType] != subProgram.ShaderHardwareTier)
+					{
+						lookup.Add(subProgram.GpuProgramType);
+					}
+				}
+				else
+				{
+					seen[subProgram.GpuProgramType] = subProgram.ShaderHardwareTier;
+				}
+			}
+			return lookup;
+		}
+
+		public IReadOnlyList<SerializedSubProgram> SubPrograms => m_subPrograms;
+
+		private SerializedSubProgram[] m_subPrograms;
+	}
 }
