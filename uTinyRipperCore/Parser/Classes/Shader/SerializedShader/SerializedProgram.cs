27ae046d81d5bf0322fd981e59eafc87f674c126
mafaca <mafaca@qq.com>
2019-11-01 11:13:49 +0300

    fixed Shader export

--- uTinyRipperCore/Parser/Classes/Shader/SerializedShader/SerializedProgram.cs
+++ uTinyRipperCore/Parser/Classes/Shader/SerializedShader/SerializedProgram.cs
@@ -9,6 +9,11 @@ namespace uTinyRipper.Classes.Shaders
 
 		public void Export(ShaderWriter writer, ShaderType type)
 		{
+			if (SubPrograms.Length == 0)
+			{
+				return;
+			}
+
 			writer.WriteIndent(3);
 			writer.Write("Program \"{0}\" {{\n", type.ToProgramTypeString());
 			int tierCount = GetTierCount(SubPrograms);
