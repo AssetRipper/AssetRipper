c41b04f6dc7d874694c995c52dd3f9bff4c0e043
mafaca <mafaca@qq.com>
2020-10-27 14:36:40 +0300

    fixed switch shader crash

--- uTinyRipperCore/Parser/Classes/Shader/Enums/GpuProgramType/ShaderGpuProgramType.cs
+++ uTinyRipperCore/Parser/Classes/Shader/Enums/GpuProgramType/ShaderGpuProgramType.cs
@@ -346,6 +346,8 @@ namespace uTinyRipper.Classes.Shaders
 									return "nssl_vs";
 								case ShaderType.Fragment:
 									return "nssl_ps";
+								case ShaderType.Geometry:
+									return "nssl_gs";
 								case ShaderType.Hull:
 									return "nssl_hs";
 							}
