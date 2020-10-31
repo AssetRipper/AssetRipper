a5a2a2a085ab64721eff2732530a1a6000061343
mafaca <mafaca@qq.com>
2020-10-31 18:55:47 +0300

    fixed random null ref exception

--- uTinyRipperCore/Utils/ThreadSafeRandom.cs
+++ uTinyRipperCore/Utils/ThreadSafeRandom.cs
@@ -4,7 +4,27 @@ namespace uTinyRipper
 {
 	public class ThreadSafeRandom
 	{
-		public ThreadSafeRandom()
+		public int Next()
+		{
+			return GetLocal().Next();
+		}
+
+		public int Next(int maxValue)
+		{
+			return GetLocal().Next(maxValue);
+		}
+
+		public int Next(int minValue, int maxValue)
+		{
+			return GetLocal().Next(minValue, maxValue);
+		}
+
+		public void NextBytes(byte[] buffer)
+		{
+			GetLocal().NextBytes(buffer);
+		}
+
+		private static Random GetLocal()
 		{
 			if (s_local == null)
 			{
@@ -15,26 +35,7 @@ namespace uTinyRipper
 				}
 				s_local = new Random(seed);
 			}
-		}
-
-		public int Next()
-		{
-			return s_local.Next();
-		}
-
-		public int Next(int maxValue)
-		{
-			return s_local.Next(maxValue);
-		}
-
-		public int Next(int minValue, int maxValue)
-		{
-			return s_local.Next(minValue, maxValue);
-		}
-
-		public void NextBytes(byte[] buffer)
-		{
-			s_local.NextBytes(buffer);
+			return s_local;
 		}
 
 		private static readonly Random s_global = new Random();
