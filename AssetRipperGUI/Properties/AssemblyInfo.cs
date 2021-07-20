e89a1bf6cf10b282c3eb44963f199aa12046551f
ds5678 <jrpritts7@gmail.com>
2021-07-19 20:39:33 -0400

    Updated project tags

--- AssetRipperGUI/Properties/AssemblyInfo.cs
+++ AssetRipperGUI/Properties/AssemblyInfo.cs
@@ -33,5 +33,5 @@ using System.Windows;
 )]
 
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
