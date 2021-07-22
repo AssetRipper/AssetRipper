71469010026e0efcab309aa9e5923d841c495496
ds5678 <jrpritts7@gmail.com>
2021-07-22 19:06:55 -0400

    Changed to No Support Exporter

--- AssetRipperCore/Converters/Project/Exporter/NoSupportExporter.cs
+++ AssetRipperCore/Converters/Project/Exporter/NoSupportExporter.cs
@@ -7,46 +7,47 @@ using Object = AssetRipper.Parser.Classes.Object.Object;
 
 namespace AssetRipper.Converters.Project.Exporter
 {
-	public class NonExporter : IAssetExporter
+	/// <summary>Designed to cause stack traces</summary>
+	public class NoSupportExporter : IAssetExporter
 	{
 		public IExportCollection CreateCollection(VirtualSerializedFile virtualFile, Object asset)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public bool Export(IExportContainer container, Object asset, string path)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public void Export(IExportContainer container, Object asset, string path, Action<IExportContainer, Object, string> callback)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public bool Export(IExportContainer container, IEnumerable<Object> assets, string path)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public void Export(IExportContainer container, IEnumerable<Object> assets, string path, Action<IExportContainer, Object, string> callback)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public bool IsHandle(Object asset, ExportOptions options)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public AssetType ToExportType(Object asset)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 
 		public bool ToUnknownExportType(ClassIDType classID, out AssetType assetType)
 		{
-			throw new NotImplementedException();
+			throw new NotSupportedException();
 		}
 	}
 }
