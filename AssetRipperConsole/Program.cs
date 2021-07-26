148d8ac56b8358fdaeb120c77a58db3f332ef9d9
Andrii Vitiv <cyberandrii@gmail.com>
2021-07-26 19:52:38 +0300

    Add command line arguments

--- AssetRipperConsole/Program.cs
+++ AssetRipperConsole/Program.cs
@@ -3,41 +3,77 @@ using AssetRipper.Logging;
 using AssetRipper.Structure.GameStructure;
 using AssetRipper.Utils;
 using AssetRipperLibrary;
+using CommandLine;
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace AssetRipperConsole
 {
 	public class Program
 	{
+		internal class Options
+		{
+			private const string DefaultLogFileName = "AssetRipperConsole.log";
+
+			[Value(0, Required = true, HelpText = "Input files or directory to export.")]
+			public IReadOnlyList<string> FilesToExport { get; set; }
+
+			[Option('o', "output", HelpText = "Directory to export to. Will be cleared if already exists.")]
+			public DirectoryInfo OutputDirectory { get; set; }
+
+			[Option("logFile", HelpText = "(Default: " + DefaultLogFileName + ") File to log to.")]
+			public FileInfo LogFile { get; set; } = new(DefaultLogFileName);
+
+			[Option('q', "quit", Default = false, HelpText = "Close console after export.")]
+			public bool Quit { get; set; }
+		}
+
 		public static void Main(string[] args)
 		{
-			if (args.Length == 0)
-			{
-				Console.WriteLine("No arguments");
-				Console.ReadKey();
-				return;
-			}
+			CommandLine.Parser.Default.ParseArguments<Options>(args)
+				.WithParsed(options =>
+				{
+					if (ValidateOptions(options))
+					{
+						Run(options);
+					}
 
-			foreach (string arg in args)
+					if (!options.Quit)
+					{
+						Console.ReadKey();
+					}
+				});
+		}
+
+		private static bool ValidateOptions(Options options)
+		{
+			foreach (string arg in options.FilesToExport)
 			{
 				if (MultiFileStream.Exists(arg))
 				{
 					continue;
 				}
+
 				if (DirectoryUtils.Exists(arg))
 				{
 					continue;
 				}
-				Console.WriteLine(MultiFileStream.IsMultiFile(arg) ?
-					$"File '{arg}' doesn't have all parts for combining" :
-					$"Neither file nor directory with path '{arg}' exists");
-				Console.ReadKey();
-				return;
+
+				Console.WriteLine(MultiFileStream.IsMultiFile(arg)
+					? $"File '{arg}' doesn't have all parts for combining"
+					: $"Neither file nor directory with path '{arg}' exists");
+
+				return false;
 			}
 
+			return true;
+		}
+
+		private static void Run(Options options)
+		{
 			Logger.Add(new ConsoleLogger());
-			Logger.Add(new FileLogger("AssetRipperConsole.log"));
+			Logger.Add(new FileLogger(options.LogFile.FullName));
 			Logger.Log(LogType.Info, LogCategory.System, "AssetRipper Console Version");
 #if PLATFORM_X64
 			Logger.LogSystemInformation("AssetRipper Console Version", "x64");
@@ -48,8 +84,8 @@ namespace AssetRipperConsole
 			try
 			{
 				Ripper ripper = new Ripper();
-				GameStructure gameStructure = ripper.Load(args);
-				string exportPath = Path.Combine("Ripped", gameStructure.Name);
+				GameStructure gameStructure = ripper.Load(options.FilesToExport);
+				string exportPath = options.OutputDirectory?.FullName ?? Path.Combine("Ripped", gameStructure.Name);
 				PrepareExportDirectory(exportPath);
 				ripper.Export(exportPath);
 			}
@@ -57,7 +93,6 @@ namespace AssetRipperConsole
 			{
 				Logger.Log(LogType.Error, LogCategory.General, ex.ToString());
 			}
-			Console.ReadKey();
 		}
 
 		private static void PrepareExportDirectory(string path)
