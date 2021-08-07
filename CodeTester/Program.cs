6b230d734ac36beb41ae5551f5eabe61049177a7
ds5678 <jrpritts7@gmail.com>
2021-08-07 17:16:01 -0400

    add code tester

---
+++ CodeTester/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CodeTester
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ CodeTester/README.md
@@ -0,0 +1,3 @@
+﻿# Code Tester
+
+Ignore this. It's just a convenient spot for me to do local code tests.
