e54776ad498868b4e45060bd23d48c4a8f0ec0f5
Jeremy Pritts <jrpritts7@gmail.com>
2021-08-13 10:33:07 -0700

    Split lines extension method

--- AssetRipperCore/Extensions/StringExtensions.cs
+++ AssetRipperCore/Extensions/StringExtensions.cs
@@ -1,10 +1,17 @@
-﻿namespace AssetRipper.Core.Extensions
+﻿using System;
+
+namespace AssetRipper.Core.Extensions
 {
 	public static class StringExtensions
 	{
 		public static bool Contains(this string _this, char c)
 		{
 			return _this.IndexOf(c) != -1;
+		}
+
+		public static string[] SplitLines(this string _this)
+		{
+			return _this.Split( new[] { "\r\n", "\r", "\n" }, StringSplitOptions.None );
 		}
 	}
 }
