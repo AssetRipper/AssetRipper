c21c00646d2a892580cd2ecc49ed38733c15c769
Jeremy Pritts <jrpritts7@gmail.com>
2021-08-29 01:02:07 -0400

    formatting

---
+++ AssetRipperCore/Project/Exporters/Engine/EngineBuiltInAsset.cs
@@ -0,0 +1,32 @@
+﻿using AssetRipper.Core.Parser.Asset;
+using AssetRipper.Core.Classes.Meta;
+using AssetRipper.Core.Classes.Misc;
+
+namespace AssetRipper.Core.Project.Exporters.Engine
+{
+	public struct EngineBuiltInAsset
+	{
+		public EngineBuiltInAsset(uint exportID, uint parameter, bool isF)
+		{
+			ExportID = exportID;
+			Parameter = parameter;
+			m_isF = isF;
+		}
+
+		public MetaPtr ToExportPointer()
+		{
+			return new MetaPtr(ExportID, GUID, AssetType.Internal);
+		}
+
+		public UnityGUID GUID => m_isF ? EngineBuiltInAssets.FGUID : EngineBuiltInAssets.EGUID;
+
+		public bool IsValid => ExportID != 0;
+		public uint ExportID { get; }
+		public uint Parameter { get; }
+
+		/// <summary>
+		///  Is assets located in DefaultResources file
+		/// </summary>
+		private readonly bool m_isF;
+	}
+}
---
+++ AssetRipperCore/Project/Exporters/Engine/EngineBuiltInAssetInfo.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityVersion = AssetRipper.Core.Parser.Files.UnityVersion;
+
+namespace AssetRipper.Core.Project.Exporters.Engine
+{
+	internal struct EngineBuiltInAssetInfo
+	{
+		public EngineBuiltInAssetInfo(UnityVersion version, EngineBuiltInAsset asset)
+		{
+			KeyValuePair<UnityVersion, EngineBuiltInAsset> kvp = new KeyValuePair<UnityVersion, EngineBuiltInAsset>(version, asset);
+			m_variations = new List<KeyValuePair<UnityVersion, EngineBuiltInAsset>>(1);
+			m_variations.Add(kvp);
+		}
+
+		public void AddVariation(UnityVersion version, EngineBuiltInAsset asset)
+		{
+			KeyValuePair<UnityVersion, EngineBuiltInAsset> kvp = new KeyValuePair<UnityVersion, EngineBuiltInAsset>(version, asset);
+			for (int i = 0; i < m_variations.Count; i++)
+			{
+				UnityVersion key = m_variations[i].Key;
+				if (key < version)
+				{
+					m_variations.Insert(i, kvp);
+					return;
+				}
+			}
+			m_variations.Add(kvp);
+		}
+
+		public bool ContainsAsset(UnityVersion version)
+		{
+			foreach (KeyValuePair<UnityVersion, EngineBuiltInAsset> kvp in m_variations)
+			{
+				if (version >= kvp.Key)
+				{
+					return true;
+				}
+			}
+			return false;
+		}
+
+		public EngineBuiltInAsset GetAsset(UnityVersion version)
+		{
+			foreach (KeyValuePair<UnityVersion, EngineBuiltInAsset> kvp in m_variations)
+			{
+				if (version >= kvp.Key)
+				{
+					return kvp.Value;
+				}
+			}
+			throw new Exception($"There is no asset for {version} version");
+		}
+
+		public bool TryGetAsset(UnityVersion version, out EngineBuiltInAsset asset)
+		{
+			foreach (KeyValuePair<UnityVersion, EngineBuiltInAsset> kvp in m_variations)
+			{
+				if (version >= kvp.Key)
+				{
+					asset = kvp.Value;
+					return true;
+				}
+			}
+			asset = default;
+			return false;
+		}
+
+		private readonly List<KeyValuePair<UnityVersion, EngineBuiltInAsset>> m_variations;
+	}
+}
