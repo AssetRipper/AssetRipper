0f424d622224494fe990b3364d459672a094ea9d
Jeremy Pritts <jrpritts7@gmail.com>
2021-09-02 00:49:00 -0400

    configuration default values

--- AssetRipperCore/Configuration/CoreConfiguration.cs
+++ AssetRipperCore/Configuration/CoreConfiguration.cs
@@ -11,14 +11,14 @@ namespace AssetRipper.Core.Configuration
 		/// <summary>
 		/// How do scripts get imported?
 		/// </summary>
-		public ScriptImportMode ScriptImportMode { get; set; } = ScriptImportMode.Default;
+		public ScriptImportMode ScriptImportMode { get; set; }
 		#endregion
 
 		#region Export Settings
 		/// <summary>
 		/// The path to create a new unity project in
 		/// </summary>
-		public string ExportPath { get; set; } = DirectoryUtils.CombineWithExecutingDirectory("Ripped");
+		public string ExportPath { get; set; }
 		/// <summary>
 		/// Should objects get exported with dependencies or without?
 		/// </summary>
@@ -31,11 +31,11 @@ namespace AssetRipper.Core.Configuration
 		/// A function to determine if an object is allowed to be exported.<br/>
 		/// Set by default to allow everything.
 		/// </summary>
-		public Func<Classes.Object.Object, bool> Filter { get; set; } = DefaultFilter;
+		public Func<Classes.Object.Object, bool> Filter { get; set; }
 		/// <summary>
 		/// How are text assets exported?
 		/// </summary>
-		public TextExportMode TextExportMode { get; set; } = TextExportMode.Parse;
+		public TextExportMode TextExportMode { get; set; }
 		#endregion
 
 		#region Project Settings
@@ -44,18 +44,32 @@ namespace AssetRipper.Core.Configuration
 		public TransferInstructionFlags Flags { get; private set; }
 		#endregion
 
+		#region Default Filter
 		/// <summary>
 		/// The default filter that allows everything
 		/// </summary>
 		public static Func<Classes.Object.Object, bool> DefaultFilter { get; } = DefaultFilterMethod;
 		private static bool DefaultFilterMethod(Classes.Object.Object asset) => true;
+		#endregion
 
-		internal void SetProjectSettings(Layout.LayoutInfo layout) => SetProjectSettings(layout.Version, layout.Platform, layout.Flags);
+		public CoreConfiguration() => ResetToDefaultValues();
+
+		internal void SetProjectSettings(Layout.LayoutInfo info) => SetProjectSettings(info.Version, info.Platform, info.Flags);
 		internal void SetProjectSettings(UnityVersion version, Platform platform, TransferInstructionFlags flags)
 		{
 			Version = version;
 			Platform = platform;
 			Flags = flags;
+		}
+
+		public virtual void ResetToDefaultValues()
+		{
+			ScriptImportMode = ScriptImportMode.Default;
+			ExportPath = DirectoryUtils.CombineWithExecutingDirectory("Ripped");
+			ExportDependencies = false;
+			KeepAssetBundleContentPath = false;
+			Filter = DefaultFilter;
+			TextExportMode = TextExportMode.Parse;
 		}
 	}
 }
--- AssetRipperLibrary/Configuration/LibraryConfiguration.cs
+++ AssetRipperLibrary/Configuration/LibraryConfiguration.cs
@@ -4,10 +4,36 @@ namespace AssetRipper.Library.Configuration
 {
 	public class LibraryConfiguration : CoreConfiguration
 	{
-		public AudioExportFormat AudioExportFormat { get; set; } = AudioExportFormat.Ogg;
-		public ImageExportFormat ImageExportFormat { get; set; } = ImageExportFormat.Png;
-		public MeshExportFormat MeshExportFormat { get; set; } = MeshExportFormat.Obj;
-		public ShaderExportMode ShaderExportMode { get; set; } = ShaderExportMode.Dummy;
-		public SpriteExportMode SpriteExportMode { get; set; } = SpriteExportMode.Native;
+		/// <summary>
+		/// The file format that audio clips get exported in. Recommended: Ogg
+		/// </summary>
+		public AudioExportFormat AudioExportFormat { get; set; } 
+		/// <summary>
+		/// The file format that images (like textures) get exported in.
+		/// </summary>
+		public ImageExportFormat ImageExportFormat { get; set; }
+		/// <summary>
+		/// The format that meshes get exported in. Currently does nothing.
+		/// </summary>
+		public MeshExportFormat MeshExportFormat { get; set; }
+		/// <summary>
+		/// Dummy shaders compile in the editor. <br/>
+		/// Disassembled shaders provide insight into their precompiled state, but don't compile in the editor.
+		/// </summary>
+		public ShaderExportMode ShaderExportMode { get; set; } 
+		/// <summary>
+		/// Should sprites be exported as a texture? Recommended: Native
+		/// </summary>
+		public SpriteExportMode SpriteExportMode { get; set; } 
+
+		public override void ResetToDefaultValues()
+		{
+			base.ResetToDefaultValues();
+			AudioExportFormat = AudioExportFormat.Ogg;
+			ImageExportFormat = ImageExportFormat.Png;
+			MeshExportFormat = MeshExportFormat.Obj;
+			ShaderExportMode = ShaderExportMode.Dummy;
+			SpriteExportMode = SpriteExportMode.Native;
+		}
 	}
 }
