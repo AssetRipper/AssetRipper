4c88d2dcea1abe28ed70d9f44b23de2d4cb1203c
Jeremy Pritts <jrpritts7@gmail.com>
2021-11-10 16:01:53 -0500

    add reflection methods for unity asset base

--- AssetRipperCommon/UnityAssetBase.cs
+++ AssetRipperCommon/UnityAssetBase.cs
@@ -8,6 +8,8 @@ using AssetRipper.Core.Project;
 using AssetRipper.Core.YAML;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace AssetRipper.Core
 {
@@ -16,6 +18,7 @@ namespace AssetRipper.Core
 	/// </summary>
 	public class UnityAssetBase : IAssetNew, IDependent
 	{
+		private const BindingFlags fieldBindingFlags = BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Instance; 
 		public UnityVersion AssetUnityVersion { get; set; }
 		public EndianType EndianType { get; set; }
 		public TransferInstructionFlags TransferInstructionFlags { get; set; }
@@ -80,6 +83,51 @@ namespace AssetRipper.Core
 		public virtual IEnumerable<PPtr<IUnityObjectBase>> FetchDependencies(DependencyContext context)
 		{
 			yield break;
+		}
+
+		private FieldInfo TryGetFieldInfo(string fieldName) => this.GetType().GetFields(fieldBindingFlags).Where(fieldInfo => fieldInfo.Name == fieldName).FirstOrDefault();
+
+		/// <summary>
+		/// Try using reflection to get the value of a field.
+		/// </summary>
+		/// <param name="fieldName">The name of the field.</param>
+		/// <returns>The boxed value of that field if it exists.</returns>
+		public object TryGetFieldValue(string fieldName) => TryGetFieldInfo(fieldName)?.GetValue(this);
+
+		/// <summary>
+		/// Try using reflection to get the value of a field.
+		/// </summary>
+		/// <typeparam name="T">The type to cast the boxed value to.</typeparam>
+		/// <param name="fieldName">The name of the field.</param>
+		/// <returns>The value of that field if it exists and is castable to the specified type.</returns>
+		public T TryGetFieldValue<T>(string fieldName)
+		{
+			object value = TryGetFieldValue(fieldName);
+			if (value is T result)
+				return result;
+			else
+				return default;
+		}
+
+		/// <summary>
+		/// Try using reflection to set the value of a field.
+		/// </summary>
+		/// <typeparam name="T">The type of the value being assigned.</typeparam>
+		/// <param name="fieldName">The name of the field.</param>
+		/// <param name="value">The value to assign.</param>
+		/// <returns>True if successful, false otherwise</returns>
+		public bool TrySetFieldValue<T>(string fieldName, T value)
+		{
+			FieldInfo fieldInfo = TryGetFieldInfo(fieldName);
+			if(fieldInfo == null)
+			{
+				return false;
+			}
+			else
+			{
+				fieldInfo.SetValue(this, value);
+				return true;
+			}
 		}
 	}
 }
