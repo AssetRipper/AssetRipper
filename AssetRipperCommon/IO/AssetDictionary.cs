5a2e1ff8afa529693db5498c1ae017d438094865
Jeremy Pritts <jrpritts7@gmail.com>
2021-12-03 16:55:56 -0500

    add asset dictionary class for representing dictionaries with non-unique keys

---
+++ AssetRipperCommon/IO/AssetDictionary.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace AssetRipper.Core.IO
+{
+	/// <summary>
+	/// A dictionary class supporting non-unique keys
+	/// </summary>
+	/// <typeparam name="TKey">The type of the keys in the dictionary.</typeparam>
+	/// <typeparam name="TValue">The type of the values in the dictionary.</typeparam>
+	public class AssetDictionary<TKey, TValue> : List<KeyValuePair<TKey, TValue>>
+	{
+		public void Add(TKey key, TValue value) => Add(new KeyValuePair<TKey, TValue>(key, value));
+	}
+}
