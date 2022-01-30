545321f56b715299450a8fdeddfe7214741048eb
Jeremy Pritts <jrpritts7@gmail.com>
2022-01-30 00:59:45 -0500

    access dictionary idea

---
+++ AssetRipperCommon/IO/DictionaryTest/AccessDictionary.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssetRipper.Core.IO.DictionaryTest
+{
+	/// <summary>
+	/// A wrapper for accessing the elements of an <see cref="NewAssetDictionary{TKey, TValue}"/>
+	/// </summary>
+	/// <typeparam name="SKey">The exposed key type</typeparam>
+	/// <typeparam name="SValue">The exposed value type</typeparam>
+	/// <typeparam name="TKey">The key type of the reference dictionary</typeparam>
+	/// <typeparam name="TValue">The value type of the reference dictionary</typeparam>
+	public class AccessDictionary<SKey, SValue, TKey, TValue> : IAccessDictionary<SKey, SValue> 
+		where TKey : class, SKey, new()
+		where TValue : class, SValue, new()
+	{
+		private readonly NewAssetDictionary<TKey, TValue> referenceDictionary;
+
+		public AccessDictionary(NewAssetDictionary<TKey, TValue> referenceDictionary)
+		{
+			this.referenceDictionary = referenceDictionary;
+		}
+
+		public int Count => referenceDictionary.Count;
+
+		public int Capacity => referenceDictionary.Capacity;
+
+		public IEnumerable<SKey> Keys => referenceDictionary.Keys;
+
+		public IEnumerable<SValue> Values => referenceDictionary.Values;
+
+		public void Add(SKey key, SValue value)
+		{
+			if (key is not TKey compatibleKey)
+			{
+				throw new ArgumentException($"{key.GetType()} is not assignable to {typeof(TKey)}", nameof(newKey));
+			}
+			if (value is not TValue compatibleValue)
+			{
+				throw new ArgumentException($"{value.GetType()} is not assignable to {typeof(TValue)}", nameof(newValue));
+			}
+			referenceDictionary.Add(compatibleKey, compatibleValue);
+		}
+
+		public void Add(KeyValuePair<SKey, SValue> pair)
+		{
+			Add(pair.Key, pair.Value);
+		}
+
+		public void AddNew() => referenceDictionary.AddNew();
+
+		public SKey GetKey(int index) => referenceDictionary.GetKey(index);
+
+		public void SetKey(int index, SKey newKey)
+		{
+			if (newKey is not TKey compatibleKey)
+			{
+				throw new ArgumentException($"{newKey.GetType()} is not assignable to {typeof(TKey)}", nameof(newKey));
+			}
+			else
+			{
+				referenceDictionary.SetKey(index, compatibleKey);
+			}
+		}
+
+		public SValue GetValue(int index) => referenceDictionary.GetValue(index);
+
+		public void SetValue(int index, SValue newValue)
+		{
+			if (newValue is not TValue compatibleValue)
+			{
+				throw new ArgumentException($"{newValue.GetType()} is not assignable to {typeof(TValue)}", nameof(newValue));
+			}
+			else
+			{
+				referenceDictionary.SetValue(index, compatibleValue);
+			}
+		}
+
+		public KeyValuePair<SKey, SValue> this[int index]
+		{
+			get => new KeyValuePair<SKey, SValue>(GetKey(index), GetValue(index));
+			set
+			{
+				SetKey(index, value.Key);
+				SetValue(index, value.Value);
+			}
+		}
+	}
+}
---
+++ AssetRipperCommon/IO/DictionaryTest/NewAssetDictionary.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AssetRipper.Core.IO.DictionaryTest
+{
+	public class NewAssetDictionary<TKey, TValue> : IAccessDictionary<TKey, TValue>
+		where TKey : class, new() 
+		where TValue : class, new()
+	{
+		private TKey[] keys;
+		private TValue[] values;
+
+		public int Count { get; private set; } = 0;
+
+		public int Capacity => keys.Length;
+
+		public IEnumerable<TKey> Keys
+		{
+			get
+			{
+				for(int i = 0; i < Count; i++)
+				{
+					yield return keys[i];
+				}
+			}
+		}
+
+		public IEnumerable<TValue> Values
+		{
+			get
+			{
+				for (int i = 0; i < Count; i++)
+				{
+					yield return values[i];
+				}
+			}
+		}
+
+		public NewAssetDictionary() : this(4) { }
+
+		public NewAssetDictionary(int capacity)
+		{
+			if(capacity == 0)
+			{
+				keys = Array.Empty<TKey>();
+				values = Array.Empty<TValue>();
+			}
+			else
+			{
+				keys = new TKey[capacity];
+				values = new TValue[capacity];
+			}
+		}
+
+		public void Add(TKey key, TValue value)
+		{
+			if(Count == Capacity)
+				Reallocate();
+			keys[Count] = key;
+			values[Count] = value;
+			Count++;
+		}
+
+		public void Add(KeyValuePair<TKey,TValue> pair)
+		{
+			Add(pair.Key, pair.Value);
+		}
+
+		public void AddNew()
+		{
+			TKey key = new();
+			TValue value = new();
+			Add(key, value);
+		}
+
+		public TKey GetKey(int index)
+		{
+			if (index < 0 || index >= Count)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			return keys[index];
+		}
+
+		public void SetKey(int index, TKey newKey)
+		{
+			if (index < 0 || index >= Count)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			keys[index] = newKey;
+		}
+
+		public TValue GetValue(int index)
+		{
+			if (index < 0 || index >= Count)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			return values[index];
+		}
+
+		public void SetValue(int index, TValue newValue)
+		{
+			if (index < 0 || index >= Count)
+				throw new ArgumentOutOfRangeException(nameof(index));
+
+			values[index] = newValue;
+		}
+
+		public KeyValuePair<TKey, TValue> this[int index]
+		{
+			get
+			{
+				if (index < 0 || index >= Count)
+					throw new ArgumentOutOfRangeException(nameof(index));
+
+				return new KeyValuePair<TKey, TValue>(keys[index], values[index]);
+			}
+			set
+			{
+				if (index < 0 || index >= Count)
+					throw new ArgumentOutOfRangeException(nameof(index));
+
+				keys[index] = value.Key;
+				values[index] = value.Value;
+			}
+		}
+
+		private void Reallocate()
+		{
+			int newSize = GetNewSize();
+			TKey[] newKeys = new TKey[newSize];
+			TValue[] newValues = new TValue[newSize];
+			Array.Copy(keys, 0, newKeys, 0, keys.Length);
+			Array.Copy(values, 0, values, 0, values.Length);
+			keys = newKeys;
+			values = newValues;
+		}
+
+		private int GetNewSize()
+		{
+			int newSize = (int)System.Math.Min(2 * (long)Capacity, int.MaxValue);
+			return System.Math.Max(newSize, 1);
+		}
+	}
+}
