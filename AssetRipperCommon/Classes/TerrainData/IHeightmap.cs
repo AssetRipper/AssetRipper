f72f4ea36794a6abc0925b7779027a03ebf7f335
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-04 01:00:40 -0500

    heightmap set properties

--- AssetRipperCommon/Classes/TerrainData/IHeightmap.cs
+++ AssetRipperCommon/Classes/TerrainData/IHeightmap.cs
@@ -4,8 +4,8 @@ namespace AssetRipper.Core.Classes.TerrainData
 {
 	public interface IHeightmap
 	{
-		int Width { get; }
-		int Height { get; }
+		int Width { get; set; }
+		int Height { get; set; }
 		short[] Heights { get; set; }
 		Vector3f Scale { get; }
 	}
