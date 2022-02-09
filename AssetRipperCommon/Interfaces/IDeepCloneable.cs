fbcd26abf930d46204f69a8b1595ab332e1159c0
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-08 21:16:23 -0500

    deep clone interface

---
+++ AssetRipperCommon/Interfaces/IDeepCloneable.cs
@@ -0,0 +1,12 @@
+﻿namespace AssetRipper.Core.Interfaces
+{
+	public interface IDeepCloneable
+	{
+		object DeepClone();
+	}
+
+	public static class DeepCloneableExtensions
+	{
+		public static T DeepClone<T>(IDeepCloneable cloneable) => (T)cloneable.DeepClone();
+	}
+}
