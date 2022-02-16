d07c6da07663670a06a2786d35ec4e55fb02c476
Jeremy Pritts <jrpritts7@gmail.com>
2022-02-16 04:42:36 -0500

    add code for a raw texture exporter

---
+++ AssetRipperLibrary/Exporters/Textures/RawTextureExportCollection.cs
@@ -0,0 +1,19 @@
+﻿using AssetRipper.Core.Classes.Texture2D;
+using AssetRipper.Core.Interfaces;
+using AssetRipper.Core.Project.Collections;
+using AssetRipper.Core.Project.Exporters;
+
+namespace AssetRipper.Library.Exporters.Textures
+{
+	internal class RawTextureExportCollection : AssetExportCollection
+	{
+		public RawTextureExportCollection(IAssetExporter assetExporter, ITexture2D asset) : base(assetExporter, asset)
+		{
+		}
+
+		protected override string GetExportExtension(IUnityObjectBase asset)
+		{
+			return ((ITexture2D)asset).TextureFormat.ToString();
+		}
+	}
+}
---
+++ AssetRipperLibrary/Exporters/Textures/RawTextureExporter.cs
@@ -0,0 +1,29 @@
+﻿using AssetRipper.Core.Classes.Texture2D;
+using AssetRipper.Core.Interfaces;
+using AssetRipper.Core.Parser.Files.SerializedFiles;
+using AssetRipper.Core.Project;
+using AssetRipper.Core.Project.Collections;
+using AssetRipper.Core.Project.Exporters;
+using System.IO;
+
+namespace AssetRipper.Library.Exporters.Textures
+{
+	internal class RawTextureExporter : BinaryAssetExporter
+	{
+		public override bool IsHandle(IUnityObjectBase asset)
+		{
+			return asset is ITexture2D;
+		}
+
+		public override bool Export(IExportContainer container, IUnityObjectBase asset, string path)
+		{
+			File.WriteAllBytes(path, ((ITexture2D)asset).GetImageData());
+			return true;
+		}
+
+		public override IExportCollection CreateCollection(VirtualSerializedFile virtualFile, IUnityObjectBase asset)
+		{
+			return new RawTextureExportCollection(this, (ITexture2D)asset);
+		}
+	}
+}
