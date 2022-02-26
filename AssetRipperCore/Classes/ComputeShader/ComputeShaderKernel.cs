71dfd8a57b7a434c57b4a8efa8f82e8766eb45c7
Jannify <23176718+Jannify@users.noreply.github.com>
2022-02-26 09:52:50 +0100

    Added ComputeShader support

---
+++ AssetRipperCore/Classes/ComputeShader/ComputeShaderKernel.cs
@@ -0,0 +1,52 @@
+﻿using AssetRipper.Core.IO.Asset;
+using AssetRipper.Core.IO.Extensions;
+using AssetRipper.Core.Project;
+using AssetRipper.Core.YAML;
+using AssetRipper.Core.YAML.Extensions;
+
+namespace AssetRipper.Core.Classes.ComputeShader
+{
+	public sealed class  ComputeShaderKernel : IAssetReadable, IYAMLExportable
+	{
+		public void Read(AssetReader reader)
+		{
+			Name = reader.ReadString();
+			Cbs = reader.ReadAssetArray<ComputeShaderResource>();
+			reader.AlignStream();
+			Textures = reader.ReadAssetArray<ComputeShaderResource>();
+			reader.AlignStream();
+			BuiltinSamplers = reader.ReadAssetArray<ComputeShaderBuiltinSampler>();
+			reader.AlignStream();
+			InBuffers = reader.ReadAssetArray<ComputeShaderResource>();
+			reader.AlignStream();
+			OutBuffers = reader.ReadAssetArray<ComputeShaderResource>();
+			reader.AlignStream();
+			Code = reader.ReadByteArray();
+			reader.AlignStream();
+			ThreadGroupSize = reader.ReadUInt32Array();
+		}
+
+		public YAMLNode ExportYAML(IExportContainer container)
+		{
+			YAMLMappingNode node = new YAMLMappingNode();
+			node.Add("name", Name);
+			node.Add("cbs", Cbs.ExportYAML(container));
+			node.Add("textures", Textures.ExportYAML(container));
+			node.Add("builtinSamplers", BuiltinSamplers.ExportYAML(container));
+			node.Add("inBuffers", InBuffers.ExportYAML(container));
+			node.Add("outBuffers", OutBuffers.ExportYAML(container));
+			node.Add("code", Code.ExportYAML());
+			node.Add("threadGroupSize", ThreadGroupSize.ExportYAML(true));
+			return node;
+		}
+
+		public string Name { get; set; }
+		public ComputeShaderResource[] Cbs { get; set; }
+		public ComputeShaderResource[] Textures { get; set; }
+		public ComputeShaderBuiltinSampler[] BuiltinSamplers { get; set; }
+		public ComputeShaderResource[] InBuffers { get; set; }
+		public ComputeShaderResource[] OutBuffers { get; set; }
+		public byte[] Code { get; set; }
+		public uint[] ThreadGroupSize { get; set; }
+	}
+}
---
+++ AssetRipperCore/Classes/ComputeShader/ComputeShaderResource.cs
@@ -0,0 +1,35 @@
+﻿using AssetRipper.Core.IO.Asset;
+using AssetRipper.Core.Project;
+using AssetRipper.Core.YAML;
+
+namespace AssetRipper.Core.Classes.ComputeShader
+{
+	public sealed class  ComputeShaderResource : IAssetReadable, IYAMLExportable
+	{
+		public void Read(AssetReader reader)
+		{
+			Name = reader.ReadString();
+			GeneratedName = reader.ReadString();
+			BindPoint = reader.ReadInt32();
+			SamplerBindPoint = reader.ReadInt32();
+			TexDimension = reader.ReadInt32();
+		}
+
+		public YAMLNode ExportYAML(IExportContainer container)
+		{
+			YAMLMappingNode node = new YAMLMappingNode();
+			node.Add("name", Name);
+			node.Add("generatedName", GeneratedName);
+			node.Add("bindPoint", BindPoint);
+			node.Add("samplerBindPoint", SamplerBindPoint);
+			node.Add("texDimension", TexDimension);
+			return node;
+		}
+
+		public string Name { get; set; }
+		public string GeneratedName { get; set; }
+		public int BindPoint { get; set; }
+		public int SamplerBindPoint { get; set; }
+		public int TexDimension { get; set; }
+	}
+}
