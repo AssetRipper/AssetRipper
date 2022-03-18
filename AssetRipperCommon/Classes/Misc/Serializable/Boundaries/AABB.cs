cc3a62f7ad06c7310a88ed59c1cbc6d7eaa7c109
Jeremy Pritts <jrpritts7@gmail.com>
2022-03-18 17:12:58 -0400

    clean up AABB code

--- AssetRipperCommon/Classes/Misc/Serializable/Boundaries/AABB.cs
+++ AssetRipperCommon/Classes/Misc/Serializable/Boundaries/AABB.cs
@@ -8,43 +8,39 @@ namespace AssetRipper.Core.Classes.Misc.Serializable.Boundaries
 	public sealed class AABB : IAsset, IAABB
 	{
 		public AABB() : this(new Vector3f(), new Vector3f()) { }
-		public AABB(Vector3f center, Vector3f extent)
+		public AABB(IVector3f center, IVector3f extent)
 		{
-			m_Center = center;
-			m_Extent = extent;
+			Center = center;
+			Extent = extent;
 		}
-
-		public AABB DeepClone() => new AABB(m_Center.DeepClone(), m_Extent.DeepClone());
 
 		public void Read(AssetReader reader)
 		{
-			m_Center.Read(reader);
-			m_Extent.Read(reader);
+			Center.Read(reader);
+			Extent.Read(reader);
 		}
 
 		public void Write(AssetWriter writer)
 		{
-			m_Center.Write(writer);
-			m_Extent.Write(writer);
+			Center.Write(writer);
+			Extent.Write(writer);
 		}
 
 		public YAMLNode ExportYAML(IExportContainer container)
 		{
 			YAMLMappingNode node = new YAMLMappingNode();
-			node.Add(CenterName, m_Center.ExportYAML(container));
-			node.Add(ExtentName, m_Extent.ExportYAML(container));
+			node.Add(CenterName, Center.ExportYAML(container));
+			node.Add(ExtentName, Extent.ExportYAML(container));
 			return node;
 		}
 
 		public override string ToString()
 		{
-			return $"C:{m_Center} E:{m_Extent}";
+			return $"C:{Center} E:{Extent}";
 		}
 
-		private Vector3f m_Center = new();
-		private Vector3f m_Extent = new();
-		public IVector3f Center => m_Center;
-		public IVector3f Extent => m_Extent;
+		public IVector3f Center { get; }
+		public IVector3f Extent { get; }
 
 		public const string CenterName = "m_Center";
 		public const string ExtentName = "m_Extent";
