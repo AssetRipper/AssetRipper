e725210ad1822c69945294a49e52abf97ab1f0bd
Panthr75 <jjireland@stu.naperville203.org>
2022-05-23 18:19:47 -0500

    Bump - Refactor CodeCleanup, add StructLayout fix

--- AssetRipperLibrary/Exporters/Scripts/ScriptDecompiler.cs
+++ AssetRipperLibrary/Exporters/Scripts/ScriptDecompiler.cs
@@ -1,6 +1,8 @@
 ﻿using AssetRipper.Core.Configuration;
+using AssetRipper.Core.Structure.Assembly;
 using AssetRipper.Core.Structure.Assembly.Managers;
 using AssetRipper.Library.Exporters.Scripts.Transform;
+using AssetRipper.Library.Exporters.Scripts.Transforms;
 using ICSharpCode.Decompiler;
 using ICSharpCode.Decompiler.CSharp;
 using ICSharpCode.Decompiler.TypeSystem;
@@ -14,25 +16,20 @@ namespace AssetRipper.Library.Exporters.Scripts
 		private readonly CecilAssemblyResolver assemblyResolver;
 		public LanguageVersion LanguageVersion { get; set; } = LanguageVersion.CSharp7_3;
 		public ScriptContentLevel ScriptContentLevel { get; set; } = ScriptContentLevel.Level2;
-		public CodeCleanupHandler CodeCleanupHandler { get; set; }
+		public ScriptingBackend ScriptingBackend { get; set; } = ScriptingBackend.Unknown;
 
-		public ScriptDecompiler(IAssemblyManager assemblyManager, CodeCleanupSettings? cleanupSettings = null) : this(new CecilAssemblyResolver(assemblyManager), cleanupSettings) { }
-		public ScriptDecompiler(AssemblyDefinition assembly, CodeCleanupSettings? cleanupSettings = null) : this(new CecilAssemblyResolver(assembly), cleanupSettings) { }
-		public ScriptDecompiler(AssemblyDefinition[] assemblies, CodeCleanupSettings? cleanupSettings = null) : this(new CecilAssemblyResolver(assemblies), cleanupSettings) { }
-		private ScriptDecompiler(CecilAssemblyResolver cecilAssemblyResolver, CodeCleanupSettings? cleanupSettings = null)
+		public ScriptDecompiler(IAssemblyManager assemblyManager) : this(new CecilAssemblyResolver(assemblyManager), assemblyManager.ScriptingBackend) { }
+		public ScriptDecompiler(AssemblyDefinition assembly, ScriptingBackend scriptingBackend) : this(new CecilAssemblyResolver(assembly), scriptingBackend) { }
+		public ScriptDecompiler(AssemblyDefinition[] assemblies, ScriptingBackend scriptingBackend) : this(new CecilAssemblyResolver(assemblies), scriptingBackend) { }
+		private ScriptDecompiler(CecilAssemblyResolver cecilAssemblyResolver, ScriptingBackend scriptingBackend)
 		{
             assemblyResolver = cecilAssemblyResolver;
-			CodeCleanupHandler = new(cleanupSettings);
-		}
-
-		public CodeCleanupSettings CodeCleanupSettings
-		{
-			get => CodeCleanupHandler.Settings;
+			ScriptingBackend = scriptingBackend;
 		}
 
 		public void DecompileWholeProject(AssemblyDefinition assembly, string outputFolder)
 		{
-			WholeAssemblyDecompiler decompiler = new WholeAssemblyDecompiler(assemblyResolver);
+			WholeAssemblyDecompiler decompiler = new(assemblyResolver);
 			decompiler.Settings.SetLanguageVersion(LanguageVersion);
 			decompiler.Settings.UseNestedDirectoriesForNamespaces = true;
 			// these settings may need to be changed later because
@@ -41,14 +38,28 @@ namespace AssetRipper.Library.Exporters.Scripts
 			decompiler.Settings.AnonymousTypes = false;
 			decompiler.Settings.AnonymousMethods = false;
 			decompiler.Settings.AsyncEnumerator = false;
-
 			decompiler.Settings.AlwaysShowEnumMemberValues = true;
 			decompiler.Settings.ShowXmlDocumentation = true;
 			if (ScriptContentLevel == ScriptContentLevel.Level1)
 			{
 				decompiler.CustomTransforms.Add(new MethodStripperTransform());
 			}
-			CodeCleanupHandler.SetupDecompiler(decompiler);
+
+			// code quality
+			decompiler.CustomTransforms.Add(new RemoveInvalidMemberTransform());
+			decompiler.CustomTransforms.Add(new FixOptionalParametersTransform());
+			decompiler.CustomTransforms.Add(new ValidateNullCastsTransform());
+			decompiler.CustomTransforms.Add(new FixExplicitInterfaceImplementationTransform());
+			decompiler.CustomTransforms.Add(new FixStructLayoutAmbiguityTransform());
+			
+			// il2cpp fixes
+			if (ScriptingBackend == ScriptingBackend.IL2Cpp && ScriptContentLevel <= ScriptContentLevel.Level2)
+			{
+				decompiler.CustomTransforms.Add(new EnsureOutParamsSetTransform());
+				decompiler.CustomTransforms.Add(new EnsureStructFieldsSetTransform());
+				decompiler.CustomTransforms.Add(new EnsureValidBaseConstructorTransform());
+			}
+
 			DecompileWholeProject(decompiler, assembly, outputFolder);
 		}
 
---
+++ AssetRipperLibrary/Exporters/Scripts/Transforms/FixStructLayoutAmbiguityTransform.cs
@@ -0,0 +1,61 @@
+﻿using AssetRipper.Core.Logging;
+using ICSharpCode.Decompiler.CSharp.Syntax;
+using ICSharpCode.Decompiler.CSharp.Transforms;
+using System.Runtime.InteropServices;
+
+namespace AssetRipper.Library.Exporters.Scripts.Transforms
+{
+	/// <summary>
+	/// Fixes ILSpy generated code which can generate <see cref="StructLayoutAttribute"/>s on
+	/// structs. For <see cref="LayoutKind.Sequential"/>, ILSpy generated the attribute as
+	/// <c>StructLayout(0, ...)</c>, which is ambiguous between
+	/// <see cref="StructLayoutAttribute(short)"/>, and
+	/// <see cref="StructLayoutAttribute(LayoutKind)"/>
+	/// </summary>
+	internal class FixStructLayoutAmbiguityTransform : DepthFirstAstVisitor, IAstTransform
+	{
+		public override void VisitTypeDeclaration(TypeDeclaration typeDeclaration)
+		{
+			base.VisitTypeDeclaration(typeDeclaration);
+			if (typeDeclaration.ClassType != ClassType.Struct)
+			{
+				return;
+			}
+
+			foreach (AttributeSection attributeSection in typeDeclaration.Attributes)
+			{
+				foreach (Attribute attribute in attributeSection.Attributes)
+				{
+					if (attribute.Type is not SimpleType attributeTypeName || attributeTypeName.Identifier != "StructLayout")
+					{
+						continue;
+					}
+
+					if (attribute.HasArgumentList)
+					{
+						Expression? firstArgument = attribute.Arguments.FirstOrNullObject();
+						if (firstArgument != null && firstArgument is PrimitiveExpression primitive &&
+							primitive.Format == LiteralFormat.DecimalNumber)
+						{
+							Logger.Info(LogCategory.Debug, "Primitive Value: " + primitive.Value);
+							if (primitive.Value.Equals(0))
+							{
+								SimpleType type = new(nameof(StructLayoutAttribute));
+								TypeReferenceExpression typeExpression = new(type);
+								MemberReferenceExpression newArgument = new(typeExpression, nameof(LayoutKind.Sequential));
+
+								attribute.Arguments.InsertBefore(newArgument, firstArgument);
+								firstArgument.Remove();
+							}
+						}
+					}
+				}
+			}
+		}
+
+		public void Run(AstNode rootNode, TransformContext context)
+		{
+			rootNode.AcceptVisitor(this);
+		}
+	}
+}
