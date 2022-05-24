7530ec1b6754fd38c94280130a10be9c1a435201
Panthr75 <jjireland@stu.naperville203.org>
2022-05-24 13:00:24 -0500

    Bump Prototype

---
+++ AssetRipperLibrary/Exporters/Scripts/Assemblies/AssemblyDefinitionExporter.cs
@@ -0,0 +1,34 @@
+﻿using AssetRipper.Core.Logging;
+using Mono.Cecil;
+using System.IO;
+using System.Text.Json;
+
+namespace AssetRipper.Library.Exporters.Scripts.Assemblies
+{
+	public static class AssemblyDefinitionExporter
+	{
+		public static void Export(AssemblyDefinition assembly, string outputFolder)
+		{
+			string assetPath = Path.Combine(outputFolder, assembly.Name.Name + ".asmdef");
+			Logger.Info(LogCategory.Export, $"Exporting assembly definition '{assembly.Name.Name}.asmdef'");
+
+			AssemblyDefinitionAsset asset = new(assembly.Name.Name);
+			foreach (AssemblyNameReference reference in assembly.MainModule.AssemblyReferences)
+			{
+				if (ReferenceAssemblies.IsReferenceAssembly(reference.Name))
+				{
+					continue;
+				}
+
+				asset.references.Add(reference.Name);
+			}
+
+			string assetData = JsonSerializer.Serialize(asset, new JsonSerializerOptions()
+			{
+				IncludeFields = true,
+				WriteIndented = true,
+			});
+			File.WriteAllText(assetPath, assetData);
+		}
+	}
+}
