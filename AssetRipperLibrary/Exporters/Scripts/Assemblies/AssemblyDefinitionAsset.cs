21ce7e0e0e1c42839701e47a1fec0d567babf48b
Panthr75 <jjireland@stu.naperville203.org>
2022-05-24 13:13:12 -0500

    Remove properties not needed

--- AssetRipperLibrary/Exporters/Scripts/Assemblies/AssemblyDefinitionAsset.cs
+++ AssetRipperLibrary/Exporters/Scripts/Assemblies/AssemblyDefinitionAsset.cs
@@ -6,15 +6,7 @@ namespace AssetRipper.Library.Exporters.Scripts.Assemblies
 	{
 		public string name;
 		public List<string> references = new();
-		public List<string> includePlatforms = new();
-		public List<string> excludePlatforms = new();
 		public bool allowUnsafeCode;
-		public bool overrideReferences;
-		public List<string> precompiledReferences = new();
-		public bool autoReferenced;
-		public List<string> defineConstraints = new();
-		public List<string> versionDefines = new();
-		public bool noEngineReferences;
 
 		public AssemblyDefinitionAsset(string name)
 		{
