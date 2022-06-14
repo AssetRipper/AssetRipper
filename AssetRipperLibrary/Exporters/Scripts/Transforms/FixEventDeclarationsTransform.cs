8648c565ba60eccfa8910a58d72c7564c89c37e3
Panthr75 <jjireland@stu.naperville203.org>
2022-06-13 20:48:49 -0500

    Fix Event Declarations

---
+++ AssetRipperLibrary/Exporters/Scripts/Transforms/FixEventDeclarationsTransform.cs
@@ -0,0 +1,49 @@
+﻿using ICSharpCode.Decompiler.CSharp.Syntax;
+using ICSharpCode.Decompiler.CSharp.Transforms;
+
+namespace AssetRipper.Library.Exporters.Scripts.Transforms
+{
+	/// <summary>
+	/// Fixed event declarations that look like:
+	/// <code>
+	/// event MyEvent
+	/// {
+	///     add;
+	///     remove;
+	/// }
+	/// </code>
+	/// And instead replaces them with:
+	/// <code>
+	/// event MyEvent;
+	/// </code>
+	/// </summary>
+	internal class FixEventDeclarationsTransform : DepthFirstAstVisitor, IAstTransform
+	{
+		private static bool AccessorHasImplementation(Accessor accessor)
+		{
+			return !accessor.IsNull && !accessor.Body.IsNull;
+		}
+
+		public override void VisitCustomEventDeclaration(CustomEventDeclaration eventDeclaration)
+		{
+			base.VisitCustomEventDeclaration(eventDeclaration);
+
+			bool hasAccessorImpl = AccessorHasImplementation(eventDeclaration.AddAccessor) || AccessorHasImplementation(eventDeclaration.RemoveAccessor);
+
+			if (!hasAccessorImpl)
+			{
+				EventDeclaration declaration = new EventDeclaration();
+				declaration.Modifiers = eventDeclaration.Modifiers;
+				declaration.ReturnType = eventDeclaration.ReturnType.Clone();
+				declaration.Variables.Add(new VariableInitializer(eventDeclaration.Name));
+				((TypeDeclaration)eventDeclaration.Parent!).Members.Add(declaration);
+				eventDeclaration.Remove();
+			}
+		}
+
+		public void Run(AstNode rootNode, TransformContext context)
+		{
+			rootNode.AcceptVisitor(this);
+		}
+	}
+}
