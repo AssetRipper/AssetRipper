d05581d12cc3d525320ca45d3e79779eb53107c2
Jeremy Pritts <jrpritts7@gmail.com>
2022-06-18 14:17:59 -0400

    delete a bunch of legacy classes

--- AssetRipperCore/Classes/LevelGameManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using AssetRipper.Core.Parser.Asset;
-
-namespace AssetRipper.Core.Classes
-{
-	public abstract class LevelGameManager : GameManager
-	{
-		protected LevelGameManager(AssetInfo assetInfo) : base(assetInfo) { }
-	}
-}
