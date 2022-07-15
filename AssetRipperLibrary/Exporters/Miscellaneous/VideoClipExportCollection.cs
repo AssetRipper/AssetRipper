1dadb71272480e68268f756dd41f586ec5d56a59
Jeremy Pritts <jrpritts7@gmail.com>
2022-07-15 12:05:05 -0400

    Fix the file extension for VideoClip assets

--- AssetRipperLibrary/Exporters/Miscellaneous/VideoClipExportCollection.cs
+++ AssetRipperLibrary/Exporters/Miscellaneous/VideoClipExportCollection.cs
@@ -17,16 +17,22 @@ namespace AssetRipper.Library.Exporters.Miscellaneous
 		{
 			if (asset is IVideoClip329 videoClip329)
 			{
-				return Path.GetExtension(videoClip329.OriginalPath_C329.String);
+				return GetExtensionFromPath(videoClip329.OriginalPath_C329.String);
 			}
 			else if (asset is IVideoClip327 videoClip327)
 			{
-				return Path.GetExtension(videoClip327.OriginalPath_C327.String);
+				return GetExtensionFromPath(videoClip327.OriginalPath_C327.String);
 			}
 			else
 			{
 				return base.GetExportExtension(asset);
 			}
+		}
+		
+		private static string GetExtensionFromPath(string path)
+		{
+			string extension = Path.GetExtension(path);
+			return string.IsNullOrEmpty(extension) ? "bytes" : extension.Substring(1);
 		}
 	}
 }
