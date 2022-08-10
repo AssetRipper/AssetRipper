873064594fd534519e304e818c181d0d2196559e
Jeremy Pritts <jrpritts7@gmail.com>
2022-08-09 23:42:08 -0400

    Add a struct for transformations

---
+++ AssetRipper.Fundamentals/Math/Transformations/Transformation.cs
@@ -0,0 +1,25 @@
+﻿using System.Numerics;
+
+namespace AssetRipper.Core.Math.Transformations
+{
+	public readonly record struct Transformation(Matrix4x4 Matrix)
+	{
+		public Transformation(Vector3 position, Quaternion rotation, Vector3 scale) : this(ConvertToMatrix(position, rotation, scale)) { }
+		public static Transformation Identity { get; } = new Transformation(Matrix4x4.Identity);
+		
+		public static Vector3 operator *(Vector3 position, Transformation transform)
+		{
+			return Vector3.Transform(position, transform.Matrix);
+		}
+		
+		public static Transformation operator *(Transformation left, Transformation right)
+		{
+			return new Transformation(left.Matrix * right.Matrix);
+		}
+		
+		private static Matrix4x4 ConvertToMatrix(Vector3 position, Quaternion rotation, Vector3 scale)
+		{
+			return Matrix4x4.CreateScale(scale) * Matrix4x4.CreateFromQuaternion(rotation) * Matrix4x4.CreateTranslation(position);
+		}
+	}
+}
---
+++ AssetRipper.Tests/TransformationTests.cs
@@ -0,0 +1,101 @@
+﻿using AssetRipper.Core.Math.Transformations;
+using System;
+using System.Numerics;
+
+namespace AssetRipper.Tests
+{
+	internal class TransformationTests
+	{
+		[Test]
+		public void MoveThenScale()
+		{
+			Transformation transformation1 = new Transformation(new Vector3(0, .5f, 0), Quaternion.Identity, Vector3.One);
+			Transformation transformation2 = new Transformation(Vector3.Zero, Quaternion.Identity, new Vector3(1, 2, 1));
+			Transformation expectedResult = new Transformation(new Vector3(0, 1, 0), Quaternion.Identity, new Vector3(1, 2, 1));
+			Assert.That(transformation1 * transformation2, Is.EqualTo(expectedResult));
+		}
+		
+		[Test]
+		public void MoveAndRotateThenScale()
+		{
+			Transformation transformation1 = new Transformation(new Vector3(0, .5f, 0), new Quaternion(0, 0, MathF.Sqrt(2) / 2, MathF.Sqrt(2) / 2), Vector3.One);
+			Transformation transformation2 = new Transformation(Vector3.Zero, Quaternion.Identity, new Vector3(1, 2, 1));
+			Transformation expectedResult = new Transformation(new Vector3(0, 1, 0), new Quaternion(0, 0, MathF.Sqrt(2) / 2, MathF.Sqrt(2) / 2), new Vector3(2, 1, 1));
+			AssertApproximatelyEqual(transformation1 * transformation2, expectedResult, 0.00001f);
+			Assert.That(!ApproximatelyEqual(transformation2 * transformation1, expectedResult, 0.00001f));
+		}
+
+		[Test]
+		public void Identity()
+		{
+			Transformation actual = new Transformation(Vector3.Zero, Quaternion.Identity, Vector3.One);
+			AssertApproximatelyEqual(actual, Transformation.Identity, 0.00001f);
+		}
+
+		private static void AssertApproximatelyEqual(Transformation actual, Transformation expected, float maxDeviation)
+		{
+			AssertApproximatelyEqual(actual.Matrix, expected.Matrix, maxDeviation);
+		}
+
+		private static void AssertApproximatelyEqual(Matrix4x4 actual, Matrix4x4 expected, float maxDeviation)
+		{
+			Assert.Multiple(() =>
+			{
+				AssertApproximatelyEqual(actual.M11, expected.M11, maxDeviation, nameof(Matrix4x4.M11));
+				AssertApproximatelyEqual(actual.M12, expected.M12, maxDeviation, nameof(Matrix4x4.M12));
+				AssertApproximatelyEqual(actual.M13, expected.M13, maxDeviation, nameof(Matrix4x4.M13));
+				AssertApproximatelyEqual(actual.M14, expected.M14, maxDeviation, nameof(Matrix4x4.M14));
+				AssertApproximatelyEqual(actual.M21, expected.M21, maxDeviation, nameof(Matrix4x4.M21));
+				AssertApproximatelyEqual(actual.M22, expected.M22, maxDeviation, nameof(Matrix4x4.M22));
+				AssertApproximatelyEqual(actual.M23, expected.M23, maxDeviation, nameof(Matrix4x4.M23));
+				AssertApproximatelyEqual(actual.M24, expected.M24, maxDeviation, nameof(Matrix4x4.M24));
+				AssertApproximatelyEqual(actual.M31, expected.M31, maxDeviation, nameof(Matrix4x4.M31));
+				AssertApproximatelyEqual(actual.M32, expected.M32, maxDeviation, nameof(Matrix4x4.M32));
+				AssertApproximatelyEqual(actual.M33, expected.M33, maxDeviation, nameof(Matrix4x4.M33));
+				AssertApproximatelyEqual(actual.M34, expected.M34, maxDeviation, nameof(Matrix4x4.M34));
+				AssertApproximatelyEqual(actual.M41, expected.M41, maxDeviation, nameof(Matrix4x4.M41));
+				AssertApproximatelyEqual(actual.M42, expected.M42, maxDeviation, nameof(Matrix4x4.M42));
+				AssertApproximatelyEqual(actual.M43, expected.M43, maxDeviation, nameof(Matrix4x4.M43));
+				AssertApproximatelyEqual(actual.M44, expected.M44, maxDeviation, nameof(Matrix4x4.M44));
+			});
+		}
+
+		private static void AssertApproximatelyEqual(float actual, float expected, float maxDeviation, string name)
+		{
+			if (ApproximatelyEqual(actual, expected, maxDeviation))
+			{
+				Assert.Fail($"{name}\nExpected: {expected}\nActual: {actual}");
+			}
+		}
+		
+		private static bool ApproximatelyEqual(Transformation actual, Transformation expected, float maxDeviation)
+		{
+			return ApproximatelyEqual(actual.Matrix, expected.Matrix, maxDeviation);
+		}
+
+		private static bool ApproximatelyEqual(Matrix4x4 actual, Matrix4x4 expected, float maxDeviation)
+		{
+			return ApproximatelyEqual(actual.M11, expected.M11, maxDeviation)
+				&& ApproximatelyEqual(actual.M12, expected.M12, maxDeviation)
+				&& ApproximatelyEqual(actual.M13, expected.M13, maxDeviation)
+				&& ApproximatelyEqual(actual.M14, expected.M14, maxDeviation)
+				&& ApproximatelyEqual(actual.M21, expected.M21, maxDeviation)
+				&& ApproximatelyEqual(actual.M22, expected.M22, maxDeviation)
+				&& ApproximatelyEqual(actual.M23, expected.M23, maxDeviation)
+				&& ApproximatelyEqual(actual.M24, expected.M24, maxDeviation)
+				&& ApproximatelyEqual(actual.M31, expected.M31, maxDeviation)
+				&& ApproximatelyEqual(actual.M32, expected.M32, maxDeviation)
+				&& ApproximatelyEqual(actual.M33, expected.M33, maxDeviation)
+				&& ApproximatelyEqual(actual.M34, expected.M34, maxDeviation)
+				&& ApproximatelyEqual(actual.M41, expected.M41, maxDeviation)
+				&& ApproximatelyEqual(actual.M42, expected.M42, maxDeviation)
+				&& ApproximatelyEqual(actual.M43, expected.M43, maxDeviation)
+				&& ApproximatelyEqual(actual.M44, expected.M44, maxDeviation);
+		}
+
+		private static bool ApproximatelyEqual(float actual, float expected, float maxDeviation)
+		{
+			return MathF.Abs(actual - expected) > maxDeviation;
+		}
+	}
+}
