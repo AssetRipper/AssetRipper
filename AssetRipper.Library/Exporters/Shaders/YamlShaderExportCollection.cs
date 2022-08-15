030f598255ea9961c07a1ef51702899a40b3ddb9
Trouger <trouger@126.com>
2022-08-15 23:31:37 +0800

    Revert "Add YAML shader post processor patch"
    This reverts commit 25cee91dff051781985d931c057a349435cae8d5.

--- AssetRipper.Library/Exporters/Shaders/YamlShaderExportCollection.cs
+++ AssetRipper.Library/Exporters/Shaders/YamlShaderExportCollection.cs
@@ -1,7 +1,5 @@
 ﻿using AssetRipper.Core.Interfaces;
-using AssetRipper.Core.Project;
 using AssetRipper.Core.Project.Collections;
-using AssetRipper.Core.Utils;
 
 namespace AssetRipper.Library.Exporters.Shaders
 {
@@ -12,13 +10,5 @@ namespace AssetRipper.Library.Exporters.Shaders
 		}
 
 		protected override string GetExportExtension(IUnityObjectBase asset) => "asset";
-		
-		protected override bool ExportInner(IProjectAssetContainer container, string filePath, string dirPath)
-		{
-			UnityPatchUtils.ApplyPatchFromManifestResource(typeof(YamlShaderExporter).Assembly, UnityPatchName, dirPath);
-			return base.ExportInner(container, filePath, dirPath);
-		}
-		
-		private const string UnityPatchName = "AssetRipper.Library.Exporters.Shaders.UnityPatch.YamlShaderPostprocessor.txt";
 	}
 }
