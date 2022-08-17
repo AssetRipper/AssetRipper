32128be5c84a0a2c69d5e86d3984750e8d8b0338
DecentM <decentm@decentm.com>
2022-08-17 22:37:06 +0300

    Make the help command print available values
    CommandLine has a way to print available values for enum type options,
    this patch makes use of that feature. Now users will get a list of
    values they could provide to enum options.

--- AssetRipper.GUI/ConsoleApp.cs
+++ AssetRipper.GUI/ConsoleApp.cs
@@ -1,12 +1,16 @@
-﻿using AssetRipper.Core.Configuration;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Diagnostics;
+
+using CommandLine;
+using CommandLine.Text;
+
+using AssetRipper.Core.Configuration;
 using AssetRipper.Core.IO.MultiFile;
 using AssetRipper.Core.Logging;
 using AssetRipper.Core.Utils;
 using AssetRipper.Library;
 using AssetRipper.Library.Configuration;
-using CommandLine;
-using System.Collections.Generic;
-using System.IO;
 
 namespace AssetRipper.GUI
 {
@@ -31,46 +35,50 @@ namespace AssetRipper.GUI
 			[Option('q', "quit", Default = false, HelpText = "Close console after export.")]
 			public bool Quit { get; set; }
 
-			[Option('b', "bundle-mode", Default = BundledAssetsExportMode.GroupByBundleName, HelpText = "Bundled asset export mode")]
+			[Option('b', "bundle-mode", Default = BundledAssetsExportMode.GroupByBundleName, HelpText = "Bundled asset export mode\n")]
 			public BundledAssetsExportMode BundledAssetsExportMode { get; set; }
 
 			[Option('g', "ignore-streaming", Default = false, HelpText = "Ignore StreamingAssets folder")]
 			public bool IgnoreStreaming { get; set; }
 
-			[Option('a', "audio-format", Default = AudioExportFormat.Default, HelpText = "Audio export format")]
+			[Option('a', "audio-format", Default = AudioExportFormat.Default, HelpText = "Audio export format\n")]
 			public AudioExportFormat AudioExportFormat { get; set; }
 
-			[Option('p', "image-format", Default = ImageExportFormat.Png, HelpText = "Image export format")]
+			[Option('p', "image-format", Default = ImageExportFormat.Png, HelpText = "Image export format\n")]
 			public ImageExportFormat ImageExportFormat { get; set; }
 
-			[Option('m', "mesh-format", Default = MeshExportFormat.Native, HelpText = "Mesh export format")]
+			[Option('m', "mesh-format", Default = MeshExportFormat.Native, HelpText = "Mesh export format\n")]
 			public MeshExportFormat MeshExportFormat { get; set; }
 
-			[Option('s', "sprite-mode", Default = SpriteExportMode.Native, HelpText = "Sprite export mode")]
+			[Option('s', "sprite-mode", Default = SpriteExportMode.Native, HelpText = "Sprite export mode\n")]
 			public SpriteExportMode SpriteExportMode { get; set; }
 
-			[Option('t', "terrain-mode", Default = TerrainExportMode.Yaml, HelpText = "Terrain export mode")]
+			[Option('t', "terrain-mode", Default = TerrainExportMode.Yaml, HelpText = "Terrain export mode\n")]
 			public TerrainExportMode TerrainExportMode { get; set; }
 
-			[Option('x', "text-mode", Default = TextExportMode.Parse, HelpText = "Text export mode")]
+			[Option('x', "text-mode", Default = TextExportMode.Parse, HelpText = "Text export mode\n")]
 			public TextExportMode TextExportMode { get; set; }
 
-			[Option('d', "shader-mode", Default = ShaderExportMode.Dummy, HelpText = "Shader export mode")]
+			[Option('d', "shader-mode", Default = ShaderExportMode.Dummy, HelpText = "Shader export mode\n")]
 			public ShaderExportMode ShaderExportMode { get; set; }
 
-			[Option('c', "script-mode", Default = ScriptExportMode.Hybrid, HelpText = "Script export mode")]
+			[Option('c', "script-mode", Default = ScriptExportMode.Hybrid, HelpText = "Script export mode\n")]
 			public ScriptExportMode ScriptExportMode { get; set; }
 
-			[Option('r', "script-level", Default = ScriptContentLevel.Level2, HelpText = "Script content level")]
+			[Option('r', "script-level", Default = ScriptContentLevel.Level2, HelpText = "Script content level\n")]
 			public ScriptContentLevel ScriptContentLevel { get; set; }
 
-			[Option('l', "script-language-version", Default = ScriptLanguageVersion.AutoSafe, HelpText = "Script language version")]
+			[Option('l', "script-language-version", Default = ScriptLanguageVersion.AutoSafe, HelpText = "Script language version\n")]
 			public ScriptLanguageVersion ScriptLanguageVersion { get; set; }
 		}
 
 		public static void ParseArgumentsAndRun(string[] args)
 		{
-			Parser.Default.ParseArguments<Options>(args)
+			Parser parser = new Parser(with => with.HelpWriter = null);
+
+			ParserResult<Options> parserResult = parser.ParseArguments<Options>(args);
+
+			parserResult
 				.WithParsed(options =>
 				{
 					if (ValidateOptions(options))
@@ -89,7 +97,32 @@ namespace AssetRipper.GUI
 				})
 				.WithNotParsed((errors) =>
 				{
-					System.Console.ReadKey();
+					HelpText? helpText = HelpText.AutoBuild(parserResult, h =>
+					{
+						Process currentProcess = Process.GetCurrentProcess();
+
+						// System.Diagnostics provides nullable interfaces, so we check all of them
+						// and skip showing the usage example if any of them are null.
+						if (
+							currentProcess != null
+							&& currentProcess.MainModule != null
+							&& !string.IsNullOrEmpty(currentProcess.MainModule.FileName)
+						)
+						{
+							string exeName = Path.GetFileName(currentProcess.MainModule.FileName);
+							h.AddPreOptionsText($"Example: {exeName} -i Games/MyGame -o Code/MyGameDecompiled");
+						}
+
+#if DEBUG
+						h.AddPreOptionsText(string.Join('\n', errors));
+#endif
+
+						h.AddEnumValuesToHelpText = true;
+						h.AdditionalNewLineAfterOption = true;
+						return h;
+					}, e => e);
+
+					Console.WriteLine(helpText);
 				});
 		}
 
