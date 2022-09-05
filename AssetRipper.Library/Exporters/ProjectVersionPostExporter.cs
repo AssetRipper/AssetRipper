d0a765a354080e0e9732272222cc4036372291cd
Jeremy Pritts <jrpritts7@gmail.com>
2022-09-05 07:39:55 -0400

    Update ProjectVersionPostExporter for Unity 5

--- AssetRipper.Library/Exporters/ProjectVersionPostExporter.cs
+++ AssetRipper.Library/Exporters/ProjectVersionPostExporter.cs
@@ -8,6 +8,7 @@ namespace AssetRipper.Library.Exporters
 	{
 		public void DoPostExport(Ripper ripper)
 		{
+			// Although Unity 4 and lower don't have this file, we leave it in anyway for user readibility.
 			SaveProjectVersion(ripper.Settings.ProjectSettingsPath, ripper.Settings.Version);
 		}
 
@@ -16,7 +17,21 @@ namespace AssetRipper.Library.Exporters
 			Directory.CreateDirectory(projectSettingsDirectory);
 			using Stream fileStream = File.Create(Path.Combine(projectSettingsDirectory, "ProjectVersion.txt"));
 			using StreamWriter writer = new InvariantStreamWriter(fileStream, new UTF8Encoding(false));
-			writer.Write($"m_EditorVersion: {version}");
+			writer.Write($"m_EditorVersion: {version}\n");
+			if (version.IsEqual(5))
+			{
+				//Unity 5 has an extra line
+				//Even on beta versions, this always seems to be zero.
+				writer.Write("m_StandardAssetsVersion: 0");
+			}
+
+			//Beginning with 2019.1.0a10, ProjectVersion.txt files have an additional line.
+			//m_EditorVersionWithRevision: 2019.4.3f1 (f880dceab6fe)
+			//The revision is always 6 bytes.
+			//It can be acquired with the FileVersionInfo class in the System.Diagnostics namespace.
+			//FileVersionInfo versionInfo = FileVersionInfo.GetVersionInfo("path/to/Unity.exe");
+			//string productVersion = versionInfo.ProductVersion; //For example: 2019.4.3f1_f880dceab6fe
+			//string revision = productVersion.Substring(productVersion.IndexOf('_') + 1);
 		}
 	}
 }
