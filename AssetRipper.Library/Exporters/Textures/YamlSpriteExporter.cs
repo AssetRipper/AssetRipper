67f4f5cb8d0161d67ec820bf845e3f7f2275e69a
trouger <trouger@126.com>
2022-10-02 18:03:02 +0800

    Fix YAML sprite pivot

--- AssetRipper.Library/Exporters/Textures/YamlSpriteExporter.cs
+++ AssetRipper.Library/Exporters/Textures/YamlSpriteExporter.cs
@@ -3,11 +3,15 @@ using AssetRipper.Core.Interfaces;
 using AssetRipper.Core.Parser.Files.SerializedFiles;
 using AssetRipper.Core.Project.Collections;
 using AssetRipper.Core.Project.Exporters;
+using AssetRipper.Core.SourceGenExtensions;
 using AssetRipper.SourceGenerated.Classes.ClassID_213;
 using AssetRipper.SourceGenerated.Classes.ClassID_687078895;
+using AssetRipper.SourceGenerated.Subclasses.Rectf;
 using AssetRipper.SourceGenerated.Subclasses.SecondarySpriteTexture;
 using AssetRipper.SourceGenerated.Subclasses.SpriteAtlasData;
 using AssetRipper.SourceGenerated.Subclasses.SpriteRenderData;
+using AssetRipper.SourceGenerated.Subclasses.Vector2f;
+using AssetRipper.SourceGenerated.Subclasses.Vector4f;
 
 namespace AssetRipper.Library.Exporters.Textures
 {
@@ -31,38 +35,51 @@ namespace AssetRipper.Library.Exporters.Textures
 			// Otherwise, if a SpriteAtlas reference is serialized into this sprite,
 			// we must recover the m_RD field of the sprite from the SpriteAtlas.
 			ISprite sprite = (ISprite)asset;
-			if (sprite.Has_SpriteAtlas_C213() && sprite.Has_RenderDataKey_C213() && sprite.SpriteAtlas_C213.TryGetAsset(sprite.SerializedFile) is { } atlas)
+			if (sprite.Has_SpriteAtlas_C213() && sprite.SpriteAtlas_C213.TryGetAsset(sprite.SerializedFile) is { } atlas)
 			{
-				ISpriteAtlasData spriteData = atlas.RenderDataMap_C687078895[sprite.RenderDataKey_C213];
-				ISpriteRenderData m_RD = sprite.RD_C213;
-				m_RD.Texture.CopyValues(ConvertPPtr(spriteData.Texture, atlas, sprite));
-				if (m_RD.Has_AlphaTexture())
+				sprite.GetSpriteCoordinatesInAtlas(atlas, out Rectf _, out Vector2f_3_5_0_f5 pivot, out Vector4f_3_5_0_f5 border);
+				if (sprite.Has_Pivot_C213())
 				{
-					m_RD.AlphaTexture.CopyValues(ConvertPPtr(spriteData.AlphaTexture, atlas, sprite));
+					sprite.Pivot_C213.CopyValues(pivot);
 				}
-				m_RD.TextureRect.CopyValues(spriteData.TextureRect);
-				m_RD.TextureRectOffset.CopyValues(spriteData.TextureRectOffset);
-				if (m_RD.Has_AtlasRectOffset() && spriteData.Has_AtlasRectOffset())
+				if (sprite.Has_Border_C213())
 				{
-					m_RD.AtlasRectOffset.CopyValues(spriteData.AtlasRectOffset);
+					sprite.Border_C213.CopyValues(border);
 				}
-				m_RD.SettingsRaw = spriteData.SettingsRaw;
-				if (m_RD.Has_UvTransform())
+
+				if (sprite.Has_RenderDataKey_C213() &&
+				    atlas.RenderDataMap_C687078895.TryGetValue(sprite.RenderDataKey_C213, out ISpriteAtlasData? spriteData))
 				{
-					m_RD.UvTransform.CopyValues(spriteData.UvTransform);
-				}
-				m_RD.DownscaleMultiplier = spriteData.DownscaleMultiplier;
-				if (m_RD.Has_SecondaryTextures() && spriteData.Has_SecondaryTextures())
-				{
-					m_RD.SecondaryTextures.Clear();
-					foreach (SecondarySpriteTexture spt in spriteData.SecondaryTextures)
+					ISpriteRenderData m_RD = sprite.RD_C213;
+					m_RD.Texture.CopyValues(ConvertPPtr(spriteData.Texture, atlas, sprite));
+					if (m_RD.Has_AlphaTexture())
 					{
-						SecondarySpriteTexture newSpt = m_RD.SecondaryTextures.AddNew();
-						newSpt.Name.CopyValues(spt.Name);
-						newSpt.Texture.CopyValues(ConvertPPtr(spt.Texture, atlas, sprite));
+						m_RD.AlphaTexture.CopyValues(ConvertPPtr(spriteData.AlphaTexture, atlas, sprite));
+					}
+					m_RD.TextureRect.CopyValues(spriteData.TextureRect);
+					m_RD.TextureRectOffset.CopyValues(spriteData.TextureRectOffset);
+					if (m_RD.Has_AtlasRectOffset() && spriteData.Has_AtlasRectOffset())
+					{
+						m_RD.AtlasRectOffset.CopyValues(spriteData.AtlasRectOffset);
+					}
+					m_RD.SettingsRaw = spriteData.SettingsRaw;
+					if (m_RD.Has_UvTransform())
+					{
+						m_RD.UvTransform.CopyValues(spriteData.UvTransform);
+					}
+					m_RD.DownscaleMultiplier = spriteData.DownscaleMultiplier;
+					if (m_RD.Has_SecondaryTextures() && spriteData.Has_SecondaryTextures())
+					{
+						m_RD.SecondaryTextures.Clear();
+						foreach (SecondarySpriteTexture spt in spriteData.SecondaryTextures)
+						{
+							SecondarySpriteTexture newSpt = m_RD.SecondaryTextures.AddNew();
+							newSpt.Name.CopyValues(spt.Name);
+							newSpt.Texture.CopyValues(ConvertPPtr(spt.Texture, atlas, sprite));
+						}
 					}
 				}
-				
+
 				// Must clear the reference to SpriteAtlas, since Unity Editor will crash trying to pack an already-packed sprite otherwise.
 				sprite.SpriteAtlas_C213.SetValues(0, 0);
 				sprite.AtlasTags_C213?.Clear();
