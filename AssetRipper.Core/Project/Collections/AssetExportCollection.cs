6de7e4be39c7a455807f8f025dd4850a2fa156f5
Jeremy Pritts <jrpritts7@gmail.com>
2022-10-05 19:21:34 -0400

    Use full path in AssetExportCollection

--- AssetRipper.Core/Project/Collections/AssetExportCollection.cs
+++ AssetRipper.Core/Project/Collections/AssetExportCollection.cs
@@ -29,7 +29,8 @@ namespace AssetRipper.Core.Project.Collections
 					: Asset.OriginalName.Length > 0
 						? Asset.OriginalName
 						: Asset.AssetClassName;
-				string resourcePath = FileUtils.FixInvalidNameCharacters(Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}"));
+				string resourcePath = Path.GetFullPath(FileUtils.FixInvalidNameCharacters(
+					Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
 				subPath = Path.GetDirectoryName(resourcePath)!;
 				string resFileName = Path.GetFileName(resourcePath);
 				fileName = GetUniqueFileName(subPath, resFileName);
