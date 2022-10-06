AssetRipper/AssetRipper

commit 6de7e4be39c7a455807f8f025dd4850a2fa156f5

Use full path in AssetExportCollection

AssetRipper.Core/Project/Collections/AssetExportCollection.cs
@@ -29,7 +29,8 @@ namespace AssetRipper.Core.Project.Collections
 					: Asset.OriginalName.Length > 0
 						? Asset.OriginalName
 						: Asset.AssetClassName;
-				string resourcePath = FileUtils.FixInvalidNameCharacters(Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}"));
+				string resourcePath = Path.GetFullPath(FileUtils.FixInvalidNameCharacters(
+					Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
 				subPath = Path.GetDirectoryName(resourcePath)!;
 				string resFileName = Path.GetFileName(resourcePath);
 				fileName = GetUniqueFileName(subPath, resFileName);

commit 49594de87916ce851709465284a43fbb6cd274cb

Fix accidental output path regression in 6de7e4be39c7a455807f8f025dd4850a2fa156f5

AssetRipper.Core/Project/Collections/AssetExportCollection.cs
@@ -29,8 +29,8 @@ namespace AssetRipper.Core.Project.Collections
 					: Asset.OriginalName.Length > 0
 						? Asset.OriginalName
 						: Asset.AssetClassName;
-				string resourcePath = Path.GetFullPath(FileUtils.FixInvalidNameCharacters(
-					Path.Combine(projectDirectory, Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
+				string resourcePath = Path.Combine(projectDirectory, DirectoryUtils.FixInvalidPathCharacters(
+					Path.Combine(Asset.OriginalDirectory ?? "", $"{assetName}.{GetExportExtension(Asset)}")));
 				subPath = Path.GetDirectoryName(resourcePath)!;
 				string resFileName = Path.GetFileName(resourcePath);
 				fileName = GetUniqueFileName(subPath, resFileName);