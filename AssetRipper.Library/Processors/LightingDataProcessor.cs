c54734b245244510fdcdc57b4b10b3af83b24c1b
Jeremy Pritts <jrpritts7@gmail.com>
2022-10-10 23:22:06 -0400

    In LightingDataProcessor, copy EnlightenSceneMapping and LightmapsMode

--- AssetRipper.Library/Processors/LightingDataProcessor.cs
+++ AssetRipper.Library/Processors/LightingDataProcessor.cs
@@ -5,11 +5,15 @@ using AssetRipper.Assets.Metadata;
 using AssetRipper.Core.Linq;
 using AssetRipper.Core.Logging;
 using AssetRipper.Core.Structure.GameStructure;
-using AssetRipper.IO.Files;
 using AssetRipper.IO.Files.SerializedFiles;
 using AssetRipper.SourceGenerated;
 using AssetRipper.SourceGenerated.Classes.ClassID_1120;
 using AssetRipper.SourceGenerated.Classes.ClassID_157;
+using AssetRipper.SourceGenerated.Subclasses.EnlightenRendererInformation;
+using AssetRipper.SourceGenerated.Subclasses.EnlightenSystemAtlasInformation;
+using AssetRipper.SourceGenerated.Subclasses.EnlightenSystemInformation;
+using AssetRipper.SourceGenerated.Subclasses.EnlightenTerrainChunksInformation;
+using AssetRipper.SourceGenerated.Subclasses.Hash128;
 using AssetRipper.SourceGenerated.Subclasses.LightmapData;
 using System.Linq;
 
@@ -38,6 +42,10 @@ namespace AssetRipper.Library.Processors
 						SetPPtr(newLightmapData.ShadowMask, processedCollection, lightmapData.ShadowMask, collection);
 					}
 
+					lightingDataAsset.LightmapsMode_C1120 = lightmapSettings.LightmapsMode_C157;
+
+					CopyEnlightenSceneMapping(lightingDataAsset, lightmapSettings);
+
 					SetPPtr(lightingDataAsset.LightProbes_C1120, processedCollection, lightmapSettings.LightProbes_C157, collection);
 
 					lightingDataAsset.EnlightenDataVersion_C1120 = 112;
@@ -62,11 +70,65 @@ namespace AssetRipper.Library.Processors
 			}
 		}
 
+		private static void CopyEnlightenSceneMapping(ILightingDataAsset lightingDataAsset, ILightmapSettings lightmapSettings)
+		{
+			if (!lightmapSettings.Has_EnlightenSceneMapping_C157())
+			{
+				return;
+			}
+
+			if (lightingDataAsset.EnlightenSceneMapping_C1120.Has_Probesets() && lightmapSettings.EnlightenSceneMapping_C157.Has_Probesets())
+			{
+				foreach (Hash128 hash in lightmapSettings.EnlightenSceneMapping_C157.Probesets)
+				{
+					lightingDataAsset.EnlightenSceneMapping_C1120.Probesets.AddNew().CopyValues(hash);
+				}
+			}
+
+			foreach (EnlightenRendererInformation renderInfo in lightmapSettings.EnlightenSceneMapping_C157.Renderers)
+			{
+				EnlightenRendererInformation dataRenderInfo = lightingDataAsset.EnlightenSceneMapping_C1120.Renderers.AddNew();
+				dataRenderInfo.DynamicLightmapSTInSystem.CopyValues(renderInfo.DynamicLightmapSTInSystem);
+				dataRenderInfo.GeometryHash.CopyValues(renderInfo.GeometryHash);
+				dataRenderInfo.InstanceHash.CopyValues(renderInfo.InstanceHash);
+				SetPPtr(dataRenderInfo.Renderer, lightingDataAsset.Collection, renderInfo.Renderer, lightmapSettings.Collection);
+				dataRenderInfo.SystemId = renderInfo.SystemId;
+			}
+
+			foreach (EnlightenSystemAtlasInformation atlasInfo in lightmapSettings.EnlightenSceneMapping_C157.SystemAtlases)
+			{
+				EnlightenSystemAtlasInformation dataAtlasInfo = lightingDataAsset.EnlightenSceneMapping_C1120.SystemAtlases.AddNew();
+				dataAtlasInfo.AtlasHash.CopyValues(atlasInfo.AtlasHash);
+				dataAtlasInfo.AtlasSize = atlasInfo.AtlasSize;
+				dataAtlasInfo.FirstSystemId = atlasInfo.FirstSystemId;
+			}
+
+			foreach (EnlightenSystemInformation systemInfo in lightmapSettings.EnlightenSceneMapping_C157.Systems)
+			{
+				EnlightenSystemInformation dataSystemInfo = lightingDataAsset.EnlightenSceneMapping_C1120.Systems.AddNew();
+				dataSystemInfo.AtlasIndex = systemInfo.AtlasIndex;
+				dataSystemInfo.AtlasOffsetX = systemInfo.AtlasOffsetX;
+				dataSystemInfo.AtlasOffsetY = systemInfo.AtlasOffsetY;
+				dataSystemInfo.InputSystemHash.CopyValues(systemInfo.InputSystemHash);
+				dataSystemInfo.RadiositySystemHash.CopyValues(systemInfo.RadiositySystemHash);
+				dataSystemInfo.RendererIndex = systemInfo.RendererIndex;
+				dataSystemInfo.RendererSize = systemInfo.RendererSize;
+			}
+
+			foreach (EnlightenTerrainChunksInformation terrainInfo in lightmapSettings.EnlightenSceneMapping_C157.TerrainChunks)
+			{
+				EnlightenTerrainChunksInformation dataTerrainInfo = lightingDataAsset.EnlightenSceneMapping_C1120.TerrainChunks.AddNew();
+				dataTerrainInfo.FirstSystemId = terrainInfo.FirstSystemId;
+				dataTerrainInfo.NumChunksInX = terrainInfo.NumChunksInX;
+				dataTerrainInfo.NumChunksInY = terrainInfo.NumChunksInY;
+			}
+		}
+
 		private static ProcessedAssetCollection CreateProcessedCollection(GameBundle gameBundle, UnityVersion projectVersion)
 		{
 			ProcessedAssetCollection processedCollection = new ProcessedAssetCollection(gameBundle);
 			processedCollection.Name = "GeneratedLightingDataAssets";
-			processedCollection.SetLayout(projectVersion, BuildTarget.NoTarget, TransferInstructionFlags.NoTransferInstructionFlags);
+			processedCollection.SetLayout(projectVersion, IO.Files.BuildTarget.NoTarget, TransferInstructionFlags.NoTransferInstructionFlags);
 			return processedCollection;
 		}
 
