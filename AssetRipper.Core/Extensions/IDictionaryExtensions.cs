d144ce4ae0aa765ec3c2a4215e3ed41ca46e0499
Jeremy Pritts <jrpritts7@gmail.com>
2022-10-13 10:26:03 -0400

    Dictionary extension methods

--- AssetRipper.Core/Extensions/IDictionaryExtensions.cs
+++ AssetRipper.Core/Extensions/IDictionaryExtensions.cs
@@ -11,5 +11,25 @@ namespace AssetRipper.Core.Extensions
 				_this.Add(argument.Key, argument.Value);
 			}
 		}
+
+		public static T2 GetOrAdd<T1, T2>(this IDictionary<T1, T2> _this, T1 key) where T1 : notnull where T2 : new()
+		{
+			if (!_this.TryGetValue(key, out T2? value))
+			{
+				value = new();
+				_this.Add(key, value);
+			}
+			return value;
+		}
+
+		public static T2 GetOrAdd<T1, T2>(this IDictionary<T1, T2> _this, T1 key, Func<T2> factory) where T1 : notnull
+		{
+			if (!_this.TryGetValue(key, out T2? value))
+			{
+				value = factory();
+				_this.Add(key, value);
+			}
+			return value;
+		}
 	}
 }
