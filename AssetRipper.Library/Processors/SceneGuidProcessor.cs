2d46f6190cb598f432092a5139fc22ceadca846f
Jeremy Pritts <jrpritts7@gmail.com>
2022-10-12 23:46:33 -0400

    Log name in TerrainTextureProcessor and SceneGuidProcessor

--- AssetRipper.Library/Processors/SceneGuidProcessor.cs
+++ AssetRipper.Library/Processors/SceneGuidProcessor.cs
@@ -2,6 +2,7 @@
 using AssetRipper.Assets.Bundles;
 using AssetRipper.Assets.Collections;
 using AssetRipper.Core.Linq;
+using AssetRipper.Core.Logging;
 using AssetRipper.Core.Structure.GameStructure;
 using AssetRipper.IO.Files;
 using AssetRipper.SourceGenerated.Classes.ClassID_29;
@@ -12,6 +13,7 @@ namespace AssetRipper.Library.Processors
 	{
 		public void Process(GameBundle gameBundle, UnityVersion projectVersion)
 		{
+			Logger.Info(LogCategory.Processing, "Scene GUID Assignment");
 			foreach (AssetCollection collection in gameBundle.FetchAssetCollections())
 			{
 				foreach (IOcclusionCullingSettings sceneSettings in collection.SelectType<IUnityObjectBase, IOcclusionCullingSettings>())
--- AssetRipper.Library/Processors/TerrainTextureProcessor.cs
+++ AssetRipper.Library/Processors/TerrainTextureProcessor.cs
@@ -2,6 +2,7 @@
 using AssetRipper.Assets.Bundles;
 using AssetRipper.Assets.Metadata;
 using AssetRipper.Core.Linq;
+using AssetRipper.Core.Logging;
 using AssetRipper.Core.Structure.GameStructure;
 using AssetRipper.SourceGenerated.Classes.ClassID_156;
 using AssetRipper.SourceGenerated.Classes.ClassID_28;
@@ -13,6 +14,7 @@ namespace AssetRipper.Library.Processors
 	{
 		public void Process(GameBundle gameBundle, UnityVersion projectVersion)
 		{
+			Logger.Info(LogCategory.Processing, "Terrain Alpha Texture Pairing");
 			foreach (ITerrainData terrainData in gameBundle.FetchAssetsInHierarchy().SelectType<IUnityObjectBase, ITerrainData>())
 			{
 				foreach (ITexture2D alphaTexture in terrainData.SplatDatabase_C156.AlphaTextures
