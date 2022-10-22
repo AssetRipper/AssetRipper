83c3d26702e7e424510ccdbf4789514135fdd831
Jeremy Pritts <jrpritts7@gmail.com>
2022-10-21 23:07:30 -0400

    Combine ColorTests

--- AssetRipper.Numerics.Tests/ColorTests.cs
+++ AssetRipper.Numerics.Tests/ColorTests.cs
@@ -1,12 +1,22 @@
-﻿namespace AssetRipper.Numerics.Tests
+﻿using System.Runtime.CompilerServices;
+
+namespace AssetRipper.Numerics.Tests
 {
 	public class ColorTests
 	{
 		[Test]
-		public void RgbaPropertyMatchesUnsafeAs()
+		public void RgbaPropertyMatchesShiftOperators()
 		{
 			Color32 color = new Color32(33, 57, 199, 255);
 			uint value = unchecked((uint)(color.R | color.G << 8 | color.B << 16 | color.A << 24));
+			Assert.That(color.Rgba, Is.EqualTo(value));
+		}
+
+		[Test]
+		public void RgbaPropertyMatchesUnsafeAs()
+		{
+			Color32 color = new Color32(33, 57, 199, 255);
+			uint value = Unsafe.As<Color32, uint>(ref color);
 			Assert.That(color.Rgba, Is.EqualTo(value));
 		}
 	}
