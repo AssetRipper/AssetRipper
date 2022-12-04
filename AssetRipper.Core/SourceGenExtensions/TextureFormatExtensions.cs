e13c7b32c524111cd3be5a33a7cf89e369a13459
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-03 19:46:09 -0500

    TextureFormatExtensions

---
+++ AssetRipper.Core/SourceGenExtensions/TextureFormatExtensions.cs
@@ -0,0 +1,11 @@
+﻿using AssetRipper.SourceGenerated.Enums;
+
+namespace AssetRipper.Core.SourceGenExtensions;
+
+public static class TextureFormatExtensions
+{
+	public static bool IsCrunched(this TextureFormat format)
+	{
+		return format is TextureFormat.DXT1Crunched or TextureFormat.DXT5Crunched or TextureFormat.ETC_RGB4Crunched or TextureFormat.ETC2_RGBA8Crunched;
+	}
+}
