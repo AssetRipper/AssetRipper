4d525ecfec311eb2956dc73508717aa5f84e2ca9
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-04 23:53:52 -0500

    Use System.Drawing and ImageSharp for BMP writing until BmpWriter can be properly investigated and tested

--- AssetRipper.Library/Utils/DirectBitmap.InternalSaveMethods.cs
+++ AssetRipper.Library/Utils/DirectBitmap.InternalSaveMethods.cs
@@ -3,6 +3,7 @@ using SixLabors.ImageSharp.PixelFormats;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
+using System.Runtime.Versioning;
 using System.Threading.Tasks;
 
 namespace AssetRipper.Library.Utils
@@ -11,26 +12,40 @@ namespace AssetRipper.Library.Utils
 	{
 		public bool SaveAsBmp(Stream stream)
 		{
-			BmpWriter.WriteBmp(Bits, Width, Height, stream);
-			return true;
+			//BmpWriter.WriteBmp(Bits, Width, Height, stream);
+			//return true;
+			if (OperatingSystem.IsWindows())
+			{
+				using Bitmap bitmap = ToSystemBitmap();
+				bitmap.Save(stream, ImageFormat.Bmp);
+				return true;
+			}
+			else
+			{
+				using Image<Bgra32> image = ToImageSharp();
+				image.SaveAsBmp(stream);
+				return true;
+			}
 		}
 
 		public async Task SaveAsBmpAsync(Stream stream)
 		{
-			await Task.Run(() => BmpWriter.WriteBmp(Bits, Width, Height, stream));
+			//await Task.Run(() => BmpWriter.WriteBmp(Bits, Width, Height, stream));
+			using Image<Bgra32> image = ToImageSharp();
+			await image.SaveAsBmpAsync(stream);
 		}
 
 		public bool SaveAsGif(Stream stream)
 		{
 			if (OperatingSystem.IsWindows())
 			{
-				using Bitmap bitmap = new Bitmap(Width, Height, Stride, PixelFormat.Format32bppArgb, m_bitsHandle.AddrOfPinnedObject());
+				using Bitmap bitmap = ToSystemBitmap();
 				bitmap.Save(stream, ImageFormat.Gif);
 				return true;
 			}
 			else
 			{
-				using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+				using Image<Bgra32> image = ToImageSharp();
 				image.SaveAsGif(stream);
 				return true;
 			}
@@ -38,7 +53,7 @@ namespace AssetRipper.Library.Utils
 
 		public async Task SaveAsGifAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsGifAsync(stream);
 		}
 
@@ -46,13 +61,13 @@ namespace AssetRipper.Library.Utils
 		{
 			if (OperatingSystem.IsWindows())
 			{
-				using Bitmap bitmap = new Bitmap(Width, Height, Stride, PixelFormat.Format32bppArgb, m_bitsHandle.AddrOfPinnedObject());
+				using Bitmap bitmap = ToSystemBitmap();
 				bitmap.Save(stream, ImageFormat.Jpeg);
 				return true;
 			}
 			else
 			{
-				using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+				using Image<Bgra32> image = ToImageSharp();
 				image.SaveAsJpeg(stream);
 				return true;
 			}
@@ -60,20 +75,20 @@ namespace AssetRipper.Library.Utils
 
 		public async Task SaveAsJpegAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsJpegAsync(stream);
 		}
 
 		public bool SaveAsPbm(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			image.SaveAsPbm(stream);
 			return true;
 		}
 
 		public async Task SaveAsPbmAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsPbmAsync(stream);
 		}
 
@@ -81,13 +96,13 @@ namespace AssetRipper.Library.Utils
 		{
 			if (OperatingSystem.IsWindows())
 			{
-				using Bitmap bitmap = new Bitmap(Width, Height, Stride, PixelFormat.Format32bppArgb, m_bitsHandle.AddrOfPinnedObject());
+				using Bitmap bitmap = ToSystemBitmap();
 				bitmap.Save(stream, ImageFormat.Png);
 				return true;
 			}
 			else
 			{
-				using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+				using Image<Bgra32> image = ToImageSharp();
 				image.SaveAsPng(stream);
 				return true;
 			}
@@ -95,20 +110,20 @@ namespace AssetRipper.Library.Utils
 
 		public async Task SaveAsPngAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsPngAsync(stream);
 		}
 
 		public bool SaveAsTga(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			image.SaveAsTga(stream);
 			return true;
 		}
 
 		public async Task SaveAsTgaAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsTgaAsync(stream);
 		}
 
@@ -116,13 +131,13 @@ namespace AssetRipper.Library.Utils
 		{
 			if (OperatingSystem.IsWindows())
 			{
-				using Bitmap bitmap = new Bitmap(Width, Height, Stride, PixelFormat.Format32bppArgb, m_bitsHandle.AddrOfPinnedObject());
+				using Bitmap bitmap = ToSystemBitmap();
 				bitmap.Save(stream, ImageFormat.Tiff);
 				return true;
 			}
 			else
 			{
-				using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+				using Image<Bgra32> image = ToImageSharp();
 				image.SaveAsTiff(stream);
 				return true;
 			}
@@ -130,21 +145,32 @@ namespace AssetRipper.Library.Utils
 
 		public async Task SaveAsTiffAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsTiffAsync(stream);
 		}
 
 		public bool SaveAsWebp(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			image.SaveAsWebp(stream);
 			return true;
 		}
 
 		public async Task SaveAsWebpAsync(Stream stream)
 		{
-			using Image<Bgra32> image = SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
+			using Image<Bgra32> image = ToImageSharp();
 			await image.SaveAsWebpAsync(stream);
+		}
+
+		[SupportedOSPlatform("windows")]
+		private Bitmap ToSystemBitmap()
+		{
+			return new Bitmap(Width, Height, Stride, PixelFormat.Format32bppArgb, m_bitsHandle.AddrOfPinnedObject());
+		}
+
+		private Image<Bgra32> ToImageSharp()
+		{
+			return SixLabors.ImageSharp.Image.LoadPixelData<Bgra32>(Bits, Width, Height);
 		}
 	}
 }
