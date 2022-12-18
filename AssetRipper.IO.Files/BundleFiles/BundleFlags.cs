124993c92173e1d3acd205cc61441f9783b2d3ac
Jeremy Pritts <jrpritts7@gmail.com>
2022-12-18 00:28:15 -0500

    Add note about chinese encryption in the BundleFlags enum

--- AssetRipper.IO.Files/BundleFiles/BundleFlags.cs
+++ AssetRipper.IO.Files/BundleFiles/BundleFlags.cs
@@ -20,12 +20,20 @@
 		/// Padding is added after blocks info, so files within asset bundles start on aligned boundaries.
 		/// </summary>
 		/// <remarks>
-		/// Introduced in 2019.4.41f1?, 2020.3.34f1, 2021.3.2f1, 2022.1.1f1 so that Switch patching works appropriately.<br/>
+		/// Introduced in 2020.3.34f1, 2021.3.2f1, 2022.1.1f1 so that Switch patching works appropriately.<br/>
 		/// <see href="https://unity3d.com/unity/whats-new/2021.3.2"/><br/>
 		/// <see href="https://issuetracker.unity3d.com/issues/files-within-assetbundles-do-not-start-on-aligned-boundaries-breaking-patching-on-nintendo-switch"/><br/>
 		/// This fix implies that loading newly generated AssetBundles will require using this new Unity editor/runtime combination. It is not backwards compatible.
 		/// </remarks>
 		BlockInfoNeedPaddingAtStart = 0x200,
+		/// <summary>
+		/// Chinese encryption flag prior to 2020.3.34f1, 2021.3.2f1, 2022.1.1f1.
+		/// </summary>
+		EncryptionOld = 0x200,
+		/// <summary>
+		/// Chinese encryption flag (presumeably) after 2020.3.34f1, 2021.3.2f1, 2022.1.1f1.
+		/// </summary>
+		EncryptionNew = 0x400,
 	}
 
 	public static class BundleFlagsExtensions
