4254d3ac085eb68ef2bf40de4abab81242f9b5c8
aldelaro5 <aldelaro5@gmail.com>
2022-12-31 20:30:12 -0500

    Add TrueTypeFontImporter support

--- AssetRipper.Library/Exporters/Miscellaneous/FontAssetExportCollection.cs
+++ AssetRipper.Library/Exporters/Miscellaneous/FontAssetExportCollection.cs
@@ -1,6 +1,13 @@
 ﻿using AssetRipper.Assets;
+using AssetRipper.Assets.Cloning;
+using AssetRipper.Assets.Export;
 using AssetRipper.Core.Project.Collections;
+using AssetRipper.Core.SourceGenExtensions;
+using AssetRipper.SourceGenerated.Classes.ClassID_1042;
 using AssetRipper.SourceGenerated.Classes.ClassID_128;
+using AssetRipper.SourceGenerated.Enums;
+using AssetRipper.SourceGenerated.Subclasses.PPtr_Font;
+using AssetRipper.SourceGenerated.Subclasses.Utf8String;
 
 namespace AssetRipper.Library.Exporters.Miscellaneous
 {
@@ -8,6 +15,43 @@ namespace AssetRipper.Library.Exporters.Miscellaneous
 	{
 		public FontAssetExportCollection(FontAssetExporter assetExporter, IUnityObjectBase asset) : base(assetExporter, asset)
 		{
+		}
+
+		protected override IUnityObjectBase CreateImporter(IExportContainer container)
+		{
+			IFont origin = (IFont)Asset;
+			ITrueTypeFontImporter instance = TrueTypeFontImporterFactory.CreateAsset(container.ExportVersion);
+			instance.FontSize_C1042 = (int)origin.FontSize_C128;
+			instance.IncludeFontData_C1042 = true;
+			instance.Style_C1042E = origin.GetDefaultStyle();
+			if (origin.FontNames_C128.Count > 0)
+			{
+				instance.FontName_C1042?.CopyValues(origin.FontNames_C128[0]);
+				foreach (Utf8String name in origin.FontNames_C128)
+				{
+					instance.FontNames_C1042.AddNew().CopyValues(name);
+				}
+			}
+			if (origin.Has_FallbackFonts_C128() && instance.Has_FallbackFontReferences_C1042())
+			{
+				PPtrConverter ptrConverter = new(origin, origin);
+				foreach (IPPtr_Font ptrFont in origin.FallbackFonts_C128)
+				{
+					instance.FallbackFontReferences_C1042.AddNew().CopyValues(ptrFont, ptrConverter);
+				}
+			}
+			instance.CharacterSpacing_C1042 = origin.CharacterSpacing_C128;
+			instance.CharacterPadding_C1042 = origin.CharacterPadding_C128;
+			instance.FontRenderingMode_C1042 = origin.FontRenderingMode_C128;
+			instance.AscentCalculationMode_C1042E = AscentCalculationMode.FaceAscender;
+			instance.ForceTextureCase_C1042E = FontTextureCase.Dynamic;
+			instance.UseLegacyBoundsCalculation_C1042 = origin.UseLegacyBoundsCalculation_C128;
+			instance.ShouldRoundAdvanceValue_C1042 = origin.ShouldRoundAdvanceValue_C128;
+			if (instance.Has_AssetBundleName_C1042() && origin.AssetBundleName is not null)
+			{
+				instance.AssetBundleName_C1042.String = origin.AssetBundleName;
+			}
+			return instance;
 		}
 
 		protected override string GetExportExtension(IUnityObjectBase asset)
