7a464007eb2fa88f040a3360dfe15cd90b713ef0
Jeremy Pritts <jrpritts7@gmail.com>
2023-01-08 20:12:20 -0500

    Update the localized string for GLB

--- AssetRipper.GUI/Components/MeshExportConfigDropdown.cs
+++ AssetRipper.GUI/Components/MeshExportConfigDropdown.cs
@@ -7,14 +7,14 @@ namespace AssetRipper.GUI.Components
 		protected override string GetValueDisplayName(MeshExportFormat value) => value switch
 		{
 			MeshExportFormat.Native => MainWindow.Instance.LocalizationManager["mesh_format_native"],
-			MeshExportFormat.Glb => MainWindow.Instance.LocalizationManager["mesh_format_glb_primitive"],
+			MeshExportFormat.Glb => MainWindow.Instance.LocalizationManager["mesh_format_glb"],
 			_ => base.GetValueDisplayName(value),
 		};
 
 		protected override string? GetValueDescription(MeshExportFormat value) => value switch
 		{
 			MeshExportFormat.Native => MainWindow.Instance.LocalizationManager["mesh_format_native_description"],
-			MeshExportFormat.Glb => MainWindow.Instance.LocalizationManager["mesh_format_glb_primitive_description"],
+			MeshExportFormat.Glb => MainWindow.Instance.LocalizationManager["mesh_format_glb_description"],
 			_ => null,
 		};
 	}
