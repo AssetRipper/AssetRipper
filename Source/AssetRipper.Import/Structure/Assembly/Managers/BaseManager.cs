AssetRipper/AssetRipper

commit 37b21878ab1dd6e685736514b779e59bce04b6af

Cache SerializableType instances. Resolves #718

Source/AssetRipper.Import/Structure/Assembly/Managers/BaseManager.cs
@@ -16,6 +16,7 @@ namespace AssetRipper.Import.Structure.Assembly.Managers
 		protected readonly Dictionary<string, AssemblyDefinition?> m_assemblies = new();
 		protected readonly Dictionary<AssemblyDefinition, Stream> m_assemblyStreams = new();
 		protected readonly Dictionary<string, bool> m_validTypes = new();
+		private readonly Dictionary<TypeDefinition, MonoType> monoTypeCache = new();
 
 		private event Action<string> m_requestAssemblyCallback;
 		private readonly Dictionary<string, SerializableType> m_serializableTypes = new();
@@ -212,7 +213,11 @@ namespace AssetRipper.Import.Structure.Assembly.Managers
 			{
 				throw new ArgumentException($"Can't find type {scriptID.UniqueName}");
 			}
-			return new MonoType(type);
+			if (monoTypeCache.TryGetValue(type, out MonoType? monoType))
+			{
+				return monoType;
+			}
+			return new MonoType(type, monoTypeCache);
 		}
 
 		internal void AddSerializableType(ITypeDefOrRef type, SerializableType scriptType)

Source/AssetRipper.Import/Structure/Assembly/Mono/MonoType.cs
@@ -6,50 +6,51 @@ using AssetRipper.SerializationLogic.Extensions;
 
 namespace AssetRipper.Import.Structure.Assembly.Mono
 {
-	internal class MonoType : SerializableType
+	internal sealed class MonoType : SerializableType
 	{
 		//From the deleted MonoFieldContext: structs are only serializable on 4.5.0 and greater.
 
 		private MonoType(ITypeDefOrRef type) : base(type.Namespace ?? "", PrimitiveType.Complex, type.Name ?? "")
 		{
-
 		}
 
-		public MonoType(TypeDefinition typeDefinition) : this((ITypeDefOrRef)typeDefinition)
+		public MonoType(TypeDefinition typeDefinition, Dictionary<TypeDefinition, MonoType> typeCache) : this(typeDefinition)
 		{
+			typeCache[typeDefinition] = this;
 			List<Field> fields = new(typeDefinition.Fields.Count); //Ensure we allocate some initial space so that we have less chance of needing to resize the list.
 			foreach ((FieldDefinition fieldDefinition, TypeSignature fieldType) in FieldQuery.GetFieldsInTypeAndBase(typeDefinition))
 			{
 				if (FieldSerializationLogic.WillUnitySerialize(fieldDefinition, fieldType))
 				{
-					fields.Add(MakeSerializableField(fieldDefinition, fieldType));
+					fields.Add(MakeSerializableField(fieldDefinition, fieldType, typeCache));
 				}
 			}
 			Fields = fields;
 		}
 
-		public MonoType(GenericInstanceTypeSignature genericInst) : this(genericInst.GenericType)
+		public MonoType(GenericInstanceTypeSignature genericInst, Dictionary<TypeDefinition, MonoType> typeCache) : this(genericInst.GenericType)
 		{
 			List<Field> fields = new();
 			foreach ((FieldDefinition fieldDefinition, TypeSignature fieldType) in FieldQuery.GetFieldsInTypeAndBase(genericInst))
 			{
 				if (FieldSerializationLogic.WillUnitySerialize(fieldDefinition, fieldType))
 				{
-					fields.Add(MakeSerializableField(fieldDefinition, fieldType));
+					fields.Add(MakeSerializableField(fieldDefinition, fieldType, typeCache));
 				}
 			}
 			Fields = fields;
 		}
 
-		private static Field MakeSerializableField(FieldDefinition fieldDefinition, TypeSignature fieldType)
+		private static Field MakeSerializableField(FieldDefinition fieldDefinition, TypeSignature fieldType, Dictionary<TypeDefinition, MonoType> typeCache)
 		{
 			return MakeSerializableField(
 				fieldDefinition.Name ?? throw new NullReferenceException(),
 				fieldType,
-				0);
+				0,
+				typeCache);
 		}
 
-		private static Field MakeSerializableField(string name, TypeSignature typeSignature, int arrayDepth)
+		private static Field MakeSerializableField(string name, TypeSignature typeSignature, int arrayDepth, Dictionary<TypeDefinition, MonoType> typeCache)
 		{
 			switch (typeSignature)
 			{
@@ -57,45 +58,49 @@ namespace AssetRipper.Import.Structure.Assembly.Mono
 					TypeDefinition typeDefinition = typeDefOrRefSignature.Type.Resolve()
 						?? throw new NullReferenceException($"Could not resolve {typeDefOrRefSignature.FullName}");
 					SerializableType fieldType;
-					if (typeDefinition.IsEnum)
+					if (typeCache.TryGetValue(typeDefinition, out MonoType? cachedMonoType))
+					{
+						fieldType = cachedMonoType;
+					}
+					else if (typeDefinition.IsEnum)
 					{
 						TypeSignature enumValueType = typeDefinition.Fields.Single(f => !f.IsStatic).Signature!.FieldType;
 						PrimitiveType primitiveType = ((CorLibTypeSignature)enumValueType).ToPrimitiveType();
-						fieldType = new SerializablePrimitiveType(primitiveType);
+						fieldType = SerializablePrimitiveType.GetOrCreate(primitiveType);
 					}
 					else if (typeDefinition.InheritsFromObject())
 					{
-						fieldType = new SerializablePointerType();
+						fieldType = SerializablePointerType.Shared;
 					}
 					else
 					{
-						fieldType = new MonoType(typeDefinition);
+						fieldType = new MonoType(typeDefinition, typeCache);
 					}
 
 					return new Field(fieldType, arrayDepth, name);
 
 				case CorLibTypeSignature corLibTypeSignature:
-					return new Field(new SerializablePrimitiveType(corLibTypeSignature.ToPrimitiveType()), arrayDepth, name);
+					return new Field(SerializablePrimitiveType.GetOrCreate(corLibTypeSignature.ToPrimitiveType()), arrayDepth, name);
 
 				case SzArrayTypeSignature szArrayTypeSignature:
-					return MakeSerializableField(name, szArrayTypeSignature.BaseType, arrayDepth + 1);
+					return MakeSerializableField(name, szArrayTypeSignature.BaseType, arrayDepth + 1, typeCache);
 
 				case GenericInstanceTypeSignature genericInstanceTypeSignature:
-					return MakeSerializableField(name, genericInstanceTypeSignature, arrayDepth);
+					return MakeSerializableField(name, genericInstanceTypeSignature, arrayDepth, typeCache);
 
 				default:
 					throw new NotSupportedException(typeSignature.FullName);
 			};
 		}
 
-		private static Field MakeSerializableField(string name, GenericInstanceTypeSignature typeSignature, int arrayDepth)
+		private static Field MakeSerializableField(string name, GenericInstanceTypeSignature typeSignature, int arrayDepth, Dictionary<TypeDefinition, MonoType> typeCache)
 		{
 			if (typeSignature.GenericType.FullName is "System.Collections.Generic.List`1")
 			{
-				return MakeSerializableField(name, typeSignature.TypeArguments[0], arrayDepth + 1);
+				return MakeSerializableField(name, typeSignature.TypeArguments[0], arrayDepth + 1, typeCache);
 			}
 
-			return new(new MonoType(typeSignature), arrayDepth, name);
+			return new(new MonoType(typeSignature, typeCache), arrayDepth, name);
 		}
 	}
 }

Source/AssetRipper.Import/Structure/Assembly/Mono/SerializablePointerType.cs
@@ -4,7 +4,9 @@ namespace AssetRipper.Import.Structure.Assembly.Mono;
 
 internal sealed class SerializablePointerType : SerializableType
 {
-	public SerializablePointerType() : base("UnityEngine", PrimitiveType.Complex, "Object")
+	public static SerializablePointerType Shared { get; } = new();
+
+	private SerializablePointerType() : base("UnityEngine", PrimitiveType.Complex, "Object")
 	{
 	}
 }

Source/AssetRipper.Import/Structure/Assembly/Mono/SerializablePrimitiveType.cs
@@ -4,7 +4,19 @@ namespace AssetRipper.Import.Structure.Assembly.Mono;
 
 internal sealed class SerializablePrimitiveType : SerializableType
 {
-	public SerializablePrimitiveType(PrimitiveType primitiveType) : base("System", primitiveType, primitiveType.ToSystemTypeName())
+	private static readonly Dictionary<PrimitiveType, SerializablePrimitiveType> cache = new();
+
+	private SerializablePrimitiveType(PrimitiveType primitiveType) : base("System", primitiveType, primitiveType.ToSystemTypeName())
 	{
+	}
+
+	public static SerializablePrimitiveType GetOrCreate(PrimitiveType primitiveType)
+	{
+		if (!cache.TryGetValue(primitiveType, out SerializablePrimitiveType? result))
+		{
+			result = new SerializablePrimitiveType(primitiveType);
+			cache.Add(primitiveType, result);
+		}
+		return result;
 	}
 }

commit 58db6c79d9745516520ee85cd8c7148d39eb2beb

Fix MonoBehaviour regression * Introduced in 37b21878ab1dd6e685736514b779e59bce04b6af * Resolves #775

Source/AssetRipper.Import/Structure/Assembly/Managers/BaseManager.cs
@@ -208,11 +208,7 @@ namespace AssetRipper.Import.Structure.Assembly.Managers
 			{
 				return sType;
 			}
-			TypeDefinition? type = FindType(scriptID);
-			if (type == null)
-			{
-				throw new ArgumentException($"Can't find type {scriptID.UniqueName}");
-			}
+			TypeDefinition type = FindType(scriptID) ?? throw new ArgumentException($"Can't find type {scriptID.UniqueName}");
 			if (monoTypeCache.TryGetValue(type, out MonoType? monoType))
 			{
 				return monoType;

Source/AssetRipper.Import/Structure/Assembly/Mono/MonoType.cs
@@ -16,7 +16,7 @@ namespace AssetRipper.Import.Structure.Assembly.Mono
 
 		public MonoType(TypeDefinition typeDefinition, Dictionary<TypeDefinition, MonoType> typeCache) : this(typeDefinition)
 		{
-			typeCache[typeDefinition] = this;
+			typeCache.Add(typeDefinition, this);
 			List<Field> fields = new(typeDefinition.Fields.Count); //Ensure we allocate some initial space so that we have less chance of needing to resize the list.
 			foreach ((FieldDefinition fieldDefinition, TypeSignature fieldType) in FieldQuery.GetFieldsInTypeAndBase(typeDefinition))
 			{
@@ -58,11 +58,7 @@ namespace AssetRipper.Import.Structure.Assembly.Mono
 					TypeDefinition typeDefinition = typeDefOrRefSignature.Type.Resolve()
 						?? throw new NullReferenceException($"Could not resolve {typeDefOrRefSignature.FullName}");
 					SerializableType fieldType;
-					if (typeCache.TryGetValue(typeDefinition, out MonoType? cachedMonoType))
-					{
-						fieldType = cachedMonoType;
-					}
-					else if (typeDefinition.IsEnum)
+					if (typeDefinition.IsEnum)
 					{
 						TypeSignature enumValueType = typeDefinition.Fields.Single(f => !f.IsStatic).Signature!.FieldType;
 						PrimitiveType primitiveType = ((CorLibTypeSignature)enumValueType).ToPrimitiveType();
@@ -71,6 +67,11 @@ namespace AssetRipper.Import.Structure.Assembly.Mono
 					else if (typeDefinition.InheritsFromObject())
 					{
 						fieldType = SerializablePointerType.Shared;
+					}
+					else if (typeCache.TryGetValue(typeDefinition, out MonoType? cachedMonoType))
+					{
+						//This needs to come after the InheritsFromObject check so that those fields get properly converted into PPtr assets.
+						fieldType = cachedMonoType;
 					}
 					else
 					{

Source/AssetRipper.Import/Structure/Assembly/Serializable/SerializableField.cs
@@ -526,5 +526,10 @@ namespace AssetRipper.Import.Structure.Assembly.Serializable
 
 		public ulong PValue { get; set; }
 		public object CValue { get; set; }
+
+		public override string ToString()
+		{
+			return CValue?.ToString() ?? PValue.ToString();
+		}
 	}
 }