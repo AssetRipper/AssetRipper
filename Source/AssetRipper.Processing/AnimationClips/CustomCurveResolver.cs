1a9dab6e9e994652f6b389ec8d1aa2af41d6a105
aldelaro5 <aldelaro5@gmail.com>
2023-08-07 20:22:34 -0400

    Restore the utinyripper switches

--- Source/AssetRipper.Processing/AnimationClips/CustomCurveResolver.cs
+++ Source/AssetRipper.Processing/AnimationClips/CustomCurveResolver.cs
@@ -203,57 +203,126 @@ namespace AssetRipper.Processing.AnimationClips
 
 				case BindingCustomType.PositionConstraint:
 					{
-						if (PositionConstraint.TryGetPath(attribute, out string? foundPath))
+						uint property = attribute & 0xF;
+						return property switch
 						{
-							return foundPath;
-						}
+							0 => "m_RestTranslation.x",
+							1 => "m_RestTranslation.y",
+							2 => "m_RestTranslation.z",
+							3 => "m_Weight",
+							4 => "m_TranslationOffset.x",
+							5 => "m_TranslationOffset.y",
+							6 => "m_TranslationOffset.z",
+							7 => "m_AffectTranslationX",
+							8 => "m_AffectTranslationY",
+							9 => "m_AffectTranslationZ",
+							10 => "m_Active",
+							11 => $"m_Sources.Array.data[{attribute >> 8}].sourceTransform",
+							12 => $"m_Sources.Array.data[{attribute >> 8}].weight",
+							_ => throw new ArgumentException($"Unknown attribute {attribute} for {type}")
+						};
 					}
-					throw new ArgumentException($"Unknown attribute {attribute} for {type}");
 
 				case BindingCustomType.RotationConstraint:
 					{
-						if (RotationConstraint.TryGetPath(attribute, out string? foundPath))
+						uint property = attribute & 0xF;
+						return property switch
 						{
-							return foundPath;
-						}
+							0 => "m_RestRotation.x",
+							1 => "m_RestRotation.y",
+							2 => "m_RestRotation.z",
+							3 => "m_Weight",
+							4 => "m_RotationOffset.x",
+							5 => "m_RotationOffset.y",
+							6 => "m_RotationOffset.z",
+							7 => "m_AffectRotationX",
+							8 => "m_AffectRotationY",
+							9 => "m_AffectRotationZ",
+							10 => "m_Active",
+							11 => $"m_Sources.Array.data[{attribute >> 8}].sourceTransform",
+							12 => $"m_Sources.Array.data[{attribute >> 8}].weight",
+							_ => throw new ArgumentException($"Unknown attribute {attribute} for {type}")
+						};
 					}
-					throw new ArgumentException($"Unknown attribute {attribute} for {type}");
 
 				case BindingCustomType.ScaleConstraint:
 					{
-						if (ScaleConstraint.TryGetPath(attribute, out string? foundPath))
+						uint property = attribute & 0xF;
+						return property switch
 						{
-							return foundPath;
-						}
+							0 => "m_ScaleAtRest.x",
+							1 => "m_ScaleAtRest.y",
+							2 => "m_ScaleAtRest.z",
+							3 => "m_Weight",
+							4 => "m_ScalingOffset.x",
+							5 => "m_ScalingOffset.y",
+							6 => "m_ScalingOffset.z",
+							7 => "m_AffectScalingX",
+							8 => "m_AffectScalingY",
+							9 => "m_AffectScalingZ",
+							10 => "m_Active",
+							11 => $"m_Sources.Array.data[{attribute >> 8}].sourceTransform",
+							12 => $"m_Sources.Array.data[{attribute >> 8}].weight",
+							_ => throw new ArgumentException($"Unknown attribute {attribute} for {type}")
+						};
 					}
-					throw new ArgumentException($"Unknown attribute {attribute} for {type}");
 
 				case BindingCustomType.AimConstraint:
 					{
-						if (AimConstraint.TryGetPath(attribute, out string? foundPath))
+						uint property = attribute & 0xF;
+						return property switch
 						{
-							return foundPath;
-						}
+							0 => "m_Weight",
+							1 => "m_AffectRotationX",
+							2 => "m_AffectRotationY",
+							3 => "m_AffectRotationZ",
+							4 => "m_Active",
+							5 => "m_WorldUpObject",
+							6 => $"m_Sources.Array.data[{attribute >> 8}].sourceTransform",
+							7 => $"m_Sources.Array.data[{attribute >> 8}].weight",
+							_ => throw new ArgumentException($"Unknown attribute {attribute} for {type}")
+						};
 					}
-					throw new ArgumentException($"Unknown attribute {attribute} for {type}");
 
 				case BindingCustomType.ParentConstraint:
 					{
-						if (ParentConstraint.TryGetPath(attribute, out string? foundPath))
+						uint property = attribute & 0xF;
+						return property switch
 						{
-							return foundPath;
-						}
+							0 => "m_Weight",
+							1 => "m_AffectTranslationX",
+							2 => "m_AffectTranslationY",
+							3 => "m_AffectTranslationZ",
+							4 => "m_AffectRotationX",
+							5 => "m_AffectRotationY",
+							6 => "m_AffectRotationZ",
+							7 => "m_Active",
+							8 => $"m_TranslationOffsets.Array.data[{attribute >> 8}].x",
+							9 => $"m_TranslationOffsets.Array.data[{attribute >> 8}].y",
+							10 => $"m_TranslationOffsets.Array.data[{attribute >> 8}].z",
+							11 => $"m_RotationOffsets.Array.data[{attribute >> 8}].x",
+							12 => $"m_RotationOffsets.Array.data[{attribute >> 8}].y",
+							13 => $"m_RotationOffsets.Array.data[{attribute >> 8}].z",
+							14 => $"m_Sources.Array.data[{attribute >> 8}].sourceTransform",
+							15 => $"m_Sources.Array.data[{attribute >> 8}].weight",
+							_ => throw new ArgumentException($"Unknown attribute {attribute} for {type}")
+						};
 					}
-					throw new ArgumentException($"Unknown attribute {attribute} for {type}");
 
 				case BindingCustomType.LookAtConstraint:
 					{
-						if (LookAtConstraint.TryGetPath(attribute, out string? foundPath))
+						uint property = attribute & 0xF;
+						return property switch
 						{
-							return foundPath;
-						}
+							0 => "m_Weight",
+							1 => "m_Active",
+							2 => "m_WorldUpObject",
+							3 => $"m_Sources.Array.data[{attribute >> 8}].sourceTransform",
+							4 => $"m_Sources.Array.data[{attribute >> 8}].weight",
+							5 => "m_Roll",
+							_ => throw new ArgumentException($"Unknown attribute {attribute} for {type}")
+						};
 					}
-					throw new ArgumentException($"Unknown attribute {attribute} for {type}");
 				
 				case BindingCustomType.Camera:
 					{
