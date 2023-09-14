75635771debf640ec1b27653a2834fc07a056809
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-09-13 20:48:12 -0400

    Disable yaml tab for unsupported assets

---
+++ Source/AssetRipper.GUI.Electron/Pages/Assets/TabHelpers.cs
@@ -0,0 +1,10 @@
+﻿namespace AssetRipper.GUI.Electron.Pages.Assets;
+
+internal static class TabHelpers
+{
+	public static string GetTabClassSet(bool disabled)
+	{
+		const string DefaultTabClassSet = "nav-link";
+		return DefaultTabClassSet + (disabled ? " disabled" : "");
+	}
+}
