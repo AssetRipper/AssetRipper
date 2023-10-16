f79ffccdeaa2131b69f222d9b731b17f00eeee33
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-10-16 18:27:05 -0400

    Basic C# property decompilation

--- Source/AssetRipper.Decompilation.CSharp/CSharpDecompiler.cs
+++ Source/AssetRipper.Decompilation.CSharp/CSharpDecompiler.cs
@@ -5,14 +5,14 @@ using System.CodeDom.Compiler;
 
 namespace AssetRipper.Decompilation.CSharp;
 
-public class CSharpDecompiler : IDefinitionVisitor<IndentedTextWriter, IndentedTextWriter>
+public class CSharpDecompiler : IDefinitionVisitor<(IndentedTextWriter, NameGenerator), IndentedTextWriter>
 {
 	public static string Decompile(TypeDefinition type)
 	{
 		StringWriter stringWriter = new();
 		IndentedTextWriter textWriter = new(stringWriter);
 		textWriter.WriteComment("This decompilation assumes that the latest C# version is being used.");
-		type.AcceptVisitor(new CSharpDecompiler(), textWriter);
+		type.AcceptVisitor(new CSharpDecompiler(), (textWriter, TypeScopedNameGenerator.Create(type)));
 		return stringWriter.ToString();
 	}
 
@@ -112,81 +112,118 @@ public class CSharpDecompiler : IDefinitionVisitor<IndentedTextWriter, IndentedT
 		return false;
 	}
 
-	IndentedTextWriter IDefinitionVisitor<IndentedTextWriter, IndentedTextWriter>.VisitType(TypeDefinition type, IndentedTextWriter state)
+	IndentedTextWriter IDefinitionVisitor<(IndentedTextWriter, NameGenerator), IndentedTextWriter>.VisitType(TypeDefinition type, (IndentedTextWriter, NameGenerator) state)
 	{
-		NameGenerator nameGenerator = TypeScopedNameGenerator.Create(type);
+		IndentedTextWriter writer = state.Item1;
+		NameGenerator nameGenerator = state.Item2;
+
 		if (!Utf8String.IsNullOrEmpty(type.Namespace))
 		{
-			state.WriteFileScopedNamespace(type.Namespace);
+			writer.WriteFileScopedNamespace(type.Namespace);
 		}
 
-		state.Write(GetAccessModifier(type));
-		state.Write(' ');
+		writer.Write(GetAccessModifier(type));
+		writer.Write(' ');
 
 		if (TryGetInheritanceModifier(type, out string? inheritanceModifier))
 		{
-			state.Write(inheritanceModifier);
-			state.Write(' ');
+			writer.Write(inheritanceModifier);
+			writer.Write(' ');
 		}
 
-		state.Write(GetTypeCategory(type));
-		state.Write(' ');
+		writer.Write(GetTypeCategory(type));
+		writer.Write(' ');
 
-		state.Write(type.Name);
+		writer.Write(type.Name);
 
 		if (type.BaseType is not null && !IsSpecialType(type.BaseType))
 		{
-			state.Write(nameGenerator.GetFullName(type.BaseType.ToTypeSignature()));
+			writer.Write(nameGenerator.GetFullName(type.BaseType.ToTypeSignature()));
 		}
-		state.WriteLine();
-		using (new CurlyBrackets(state))
+		writer.WriteLine();
+		using (new CurlyBrackets(writer))
 		{
 			foreach (TypeDefinition nestedType in type.NestedTypes)
 			{
-				nestedType.AcceptVisitor(this, state)
+				nestedType.AcceptVisitor(this, (writer, TypeScopedNameGenerator.Create(nestedType)))
 					.WriteLineNoTabs();
 			}
 			if (type.Fields.Count > 0)
 			{
-				state.WriteComment("Field decompilation not implemented yet");
-				state.WriteLineNoTabs();
+				writer.WriteComment("Field decompilation not implemented yet");
+				writer.WriteLineNoTabs();
 			}
 			if (type.Methods.Count > 0)
 			{
-				state.WriteComment("Method decompilation not implemented yet");
-				state.WriteLineNoTabs();
+				writer.WriteComment("Method decompilation not implemented yet");
+				writer.WriteLineNoTabs();
 			}
-			if (type.Properties.Count > 0)
+			foreach (PropertyDefinition property in type.Properties)
 			{
-				state.WriteComment("Property decompilation not implemented yet");
-				state.WriteLineNoTabs();
+				property.AcceptVisitor(this, state)
+					.WriteLineNoTabs();
 			}
 			if (type.Events.Count > 0)
 			{
-				state.WriteComment("Event decompilation not implemented yet");
-				state.WriteLineNoTabs();
+				writer.WriteComment("Event decompilation not implemented yet");
+				writer.WriteLineNoTabs();
 			}
 		}
-		return state;
+		return writer;
 	}
 
-	IndentedTextWriter IDefinitionVisitor<IndentedTextWriter, IndentedTextWriter>.VisitField(FieldDefinition field, IndentedTextWriter state)
+	IndentedTextWriter IDefinitionVisitor<(IndentedTextWriter, NameGenerator), IndentedTextWriter>.VisitField(FieldDefinition field, (IndentedTextWriter, NameGenerator) state)
 	{
 		throw new NotImplementedException();
 	}
 
-	IndentedTextWriter IDefinitionVisitor<IndentedTextWriter, IndentedTextWriter>.VisitMethod(MethodDefinition method, IndentedTextWriter state)
+	IndentedTextWriter IDefinitionVisitor<(IndentedTextWriter, NameGenerator), IndentedTextWriter>.VisitMethod(MethodDefinition method, (IndentedTextWriter, NameGenerator) state)
 	{
 		throw new NotImplementedException();
 	}
 
-	IndentedTextWriter IDefinitionVisitor<IndentedTextWriter, IndentedTextWriter>.VisitEvent(EventDefinition @event, IndentedTextWriter state)
+	IndentedTextWriter IDefinitionVisitor<(IndentedTextWriter, NameGenerator), IndentedTextWriter>.VisitEvent(EventDefinition @event, (IndentedTextWriter, NameGenerator) state)
 	{
 		throw new NotImplementedException();
 	}
 
-	IndentedTextWriter IDefinitionVisitor<IndentedTextWriter, IndentedTextWriter>.VisitProperty(PropertyDefinition property, IndentedTextWriter state)
+	IndentedTextWriter IDefinitionVisitor<(IndentedTextWriter, NameGenerator), IndentedTextWriter>.VisitProperty(PropertyDefinition property, (IndentedTextWriter, NameGenerator) state)
 	{
-		throw new NotImplementedException();
+		IndentedTextWriter writer = state.Item1;
+		NameGenerator nameGenerator = state.Item2;
+
+		writer.WriteComment("Accessibly modifier not implemented yet");
+		writer.Write("public ");
+
+		if (property.GetMethod?.IsStatic ?? property.SetMethod?.IsStatic ?? false)
+		{
+			writer.Write("static ");
+		}
+
+		if (property.Signature is null)
+		{
+			writer.Write("/* Could not determine property type */ ");
+		}
+		else
+		{
+			writer.Write(nameGenerator.GetFullName(property.Signature.ReturnType));
+			writer.Write(' ');
+		}
+
+		writer.WriteLine(property.Name);
+		using (new CurlyBrackets(writer))
+		{
+			writer.WriteComment("Accessor decompilation not implemented yet.");
+			if (property.GetMethod is not null)
+			{
+				writer.WriteLine("get => default;");
+			}
+			if (property.SetMethod is not null)
+			{
+				writer.WriteLine("set { }");
+			}
+		}
+
+		return writer;
 	}
 }
