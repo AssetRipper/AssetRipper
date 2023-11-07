077ca634e033f40f0d3d1c69ded2f7617dff1bab
Jeremy Pritts <49847914+ds5678@users.noreply.github.com>
2023-11-06 20:52:37 -0500

    Add source generator project for C# decompilation

---
+++ Source/AssetRipper.Decompilation.CSharp.SourceGenerator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Reflection.Emit;
+
+namespace AssetRipper.Decompilation.CSharp.SourceGenerator;
+
+internal static class Program
+{
+	static void Main(string[] args)
+	{
+		List<OpCode> opCodes = GetOpCodes().OrderBy(c => c.Value).ToList();
+		Console.WriteLine("Done!");
+	}
+
+	/// <summary>
+	/// Gets all opcodes from <see cref="OpCodes"/> class.
+	/// </summary>
+	/// <remarks>
+	/// <see href="https://learn.microsoft.com/en-us/dotnet/api/system.reflection.emit.opcodes" /><br />
+	/// <see href="https://learn.microsoft.com/en-us/dotnet/api/system.reflection.emit.opcode" />
+	/// </remarks>
+	/// <returns>An enumeration of all the <see cref="OpCode"/>s.</returns>
+	static IEnumerable<OpCode> GetOpCodes()
+	{
+		foreach (FieldInfo field in typeof(OpCodes).GetFields(BindingFlags.Public | BindingFlags.Static))
+		{
+			if (field.FieldType == typeof(OpCode))
+			{
+				object fieldValue = field.GetValue(null) ?? throw new NullReferenceException();
+				yield return (OpCode)fieldValue;
+			}
+		}
+	}
+}
