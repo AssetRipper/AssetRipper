9ec0d25c337a2fec4f1ebe355de0b62e42adbc48
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-01-30 12:30:09 -0500

    Use TryAdd when adding default dependencies to the package manifest

--- Source/AssetRipper.Export.UnityProjects/Project/PackageManifest.cs
+++ Source/AssetRipper.Export.UnityProjects/Project/PackageManifest.cs
@@ -26,39 +26,39 @@ public record class PackageManifest([property: JsonPropertyName("dependencies")]
 	{
 		// This should be accurate to at least 2023
 
-		Dependencies.Add("com.unity.modules.ai", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.ai", "1.0.0");
 		if (version.GreaterThanOrEquals(2019, 2))
 		{
-			Dependencies.Add("com.unity.modules.androidjni", "1.0.0");
+			Dependencies.TryAdd("com.unity.modules.androidjni", "1.0.0");
 		}
-		Dependencies.Add("com.unity.modules.animation", "1.0.0");
-		Dependencies.Add("com.unity.modules.assetbundle", "1.0.0");
-		Dependencies.Add("com.unity.modules.audio", "1.0.0");
-		Dependencies.Add("com.unity.modules.cloth", "1.0.0");
-		Dependencies.Add("com.unity.modules.director", "1.0.0");
-		Dependencies.Add("com.unity.modules.imageconversion", "1.0.0");
-		Dependencies.Add("com.unity.modules.imgui", "1.0.0");
-		Dependencies.Add("com.unity.modules.jsonserialize", "1.0.0");
-		Dependencies.Add("com.unity.modules.particlesystem", "1.0.0");
-		Dependencies.Add("com.unity.modules.physics", "1.0.0");
-		Dependencies.Add("com.unity.modules.physics2d", "1.0.0");
-		Dependencies.Add("com.unity.modules.screencapture", "1.0.0");
-		Dependencies.Add("com.unity.modules.terrain", "1.0.0");
-		Dependencies.Add("com.unity.modules.terrainphysics", "1.0.0");
-		Dependencies.Add("com.unity.modules.tilemap", "1.0.0");
-		Dependencies.Add("com.unity.modules.ui", "1.0.0");
-		Dependencies.Add("com.unity.modules.uielements", "1.0.0");
-		Dependencies.Add("com.unity.modules.umbra", "1.0.0");
-		Dependencies.Add("com.unity.modules.unityanalytics", "1.0.0");
-		Dependencies.Add("com.unity.modules.unitywebrequest", "1.0.0");
-		Dependencies.Add("com.unity.modules.unitywebrequestassetbundle", "1.0.0");
-		Dependencies.Add("com.unity.modules.unitywebrequestaudio", "1.0.0");
-		Dependencies.Add("com.unity.modules.unitywebrequesttexture", "1.0.0");
-		Dependencies.Add("com.unity.modules.unitywebrequestwww", "1.0.0");
-		Dependencies.Add("com.unity.modules.vehicles", "1.0.0");
-		Dependencies.Add("com.unity.modules.video", "1.0.0");
-		Dependencies.Add("com.unity.modules.vr", "1.0.0");
-		Dependencies.Add("com.unity.modules.wind", "1.0.0");
-		Dependencies.Add("com.unity.modules.xr", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.animation", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.assetbundle", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.audio", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.cloth", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.director", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.imageconversion", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.imgui", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.jsonserialize", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.particlesystem", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.physics", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.physics2d", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.screencapture", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.terrain", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.terrainphysics", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.tilemap", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.ui", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.uielements", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.umbra", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.unityanalytics", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.unitywebrequest", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.unitywebrequestassetbundle", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.unitywebrequestaudio", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.unitywebrequesttexture", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.unitywebrequestwww", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.vehicles", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.video", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.vr", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.wind", "1.0.0");
+		Dependencies.TryAdd("com.unity.modules.xr", "1.0.0");
 	}
 }
