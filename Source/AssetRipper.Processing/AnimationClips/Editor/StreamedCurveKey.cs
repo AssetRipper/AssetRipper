bc861df74165711498b6d3bca612c2249821d384
FACS01-01 <75183551+FACS01-01@users.noreply.github.com>
2024-04-30 02:09:21 -0400

    AnimationClip Slope Fixes (#1321)
    * - only use Negative Infinity slopes on Unity>2021
    - CalculateSlopes cleanup
    
    * typo
    
    * cleanup
    
    * stricter rounding error

--- Source/AssetRipper.Processing/AnimationClips/Editor/StreamedCurveKey.cs
+++ Source/AssetRipper.Processing/AnimationClips/Editor/StreamedCurveKey.cs
@@ -10,7 +10,8 @@ namespace AssetRipper.Processing.AnimationClips.Editor
 		{
 			Index = index;
 			Coefficient = coefficient;
-			RightSidedLimit = LeftSidedLimit = value;
+			RightSidedLimit = value;
+			LeftSidedLimit = RightSidedLimit;
 		}
 
 		public void Read(ref EndianSpanReader reader)
@@ -21,53 +22,126 @@ namespace AssetRipper.Processing.AnimationClips.Editor
 			LeftSidedLimit = RightSidedLimit;
 		}
 
-		public void CalculateSlopes(float time, float nextTime, StreamedCurveKey nextKey)
+		public void CalculateSlopes(float time, float nextTime, StreamedCurveKey nextKey, bool UseNegInfSlopes)
 		{
 			if (Coefficient != default) // != (0,0,0) => regular slopes
 			{
-				OutSlope = Coefficient.Z;
-				float deltaX = nextTime - time;
-				float _CoeffX = Coefficient.X * deltaX * deltaX;
-				float _CoeffY = Coefficient.Y * deltaX;
-				nextKey.InSlope = 3 * _CoeffX + 2 * _CoeffY + Coefficient.Z; // may need some kind of rounding if wanting to get an inSlope of exactly 0
-				//nextKey.inSlope = float.Round(nextKey.inSlope, 4); // amount of decimal places may be more dynamic
-				if (nextKey.Coefficient == default) // calculate LSL only if next slope calculation needs it
+				double deltaTime = (double)nextTime - time;
+				SetRegularSlopes(deltaTime, nextKey, out double _CoeffX, out double _CoeffY);
+
+				if (UseNegInfSlopes && nextKey.Coefficient == default) // calculate next LSL only if next slope calculation needs it
 				{
-					nextKey.LeftSidedLimit = (_CoeffX + _CoeffY + Coefficient.Z) * deltaX + RightSidedLimit;
+					nextKey.LeftSidedLimit = (_CoeffX + _CoeffY + Coefficient.Z) * deltaTime + RightSidedLimit;
+					nextKey.ExactLeftSidedLimit = false;
 				}
-				return;
 			}
-			if (RightSidedLimit != LeftSidedLimit)
+
+			// set Special slope to this.OutSlope, because has priority over nextKey.InSlope on shaping the curve
+			// nextKey.InSlope can stay Zero
+
+			else if (UseNegInfSlopes)
 			{
-				// check percentage difference
-				float diff = RightSidedLimit > LeftSidedLimit ? RightSidedLimit - LeftSidedLimit : LeftSidedLimit - RightSidedLimit;
-				float AbsVal = RightSidedLimit < 0 ? -RightSidedLimit : RightSidedLimit;
-				float AbsLSV = LeftSidedLimit < 0 ? -LeftSidedLimit : LeftSidedLimit;
-				float div = AbsVal > AbsLSV ? AbsVal : AbsLSV;
-				const float ROUNDING_ERROR = 1e-5f; // arbitrary small value
-				// normally the difference between Right and Left Sided Limits should be "big"/much greater than a rounding error.
-				// if the check fails and INCORRECTLY skips this IF block, the next statements
-				// will still produce a good approximation for the expected curve
-				if (diff/div > ROUNDING_ERROR)
-				{
-					OutSlope = float.NegativeInfinity;
-					nextKey.InSlope = 0f;
-					nextKey.LeftSidedLimit = RightSidedLimit;
-					RightSidedLimit = LeftSidedLimit;
-					return;
-				}
+				// * there is one curve configuration that can fail and set incorrect slopes
+				CalculateSpecialSlopes(nextKey); // Infinity, -Infinity or Zero
 			}
+			else
+			{
+				// should be a perfect reconstruction
+				SetNonNegSpecialSlopes(nextKey); // Infinity or Zero
+			}
+		}
+
+		private void SetRegularSlopes(double deltaX, StreamedCurveKey nextKey, out double _CoeffX, out double _CoeffY)
+		{
+			OutSlope = Coefficient.Z;
+			_CoeffX = Coefficient.X * deltaX * deltaX;
+			_CoeffY = Coefficient.Y * deltaX;
+			nextKey.InSlope = (float)(3 * _CoeffX + 2 * _CoeffY + Coefficient.Z); // may need some kind of rounding if wanting to get an inSlope of exactly 0
+		}
+
+		private void SetNonNegSpecialSlopes(StreamedCurveKey nextKey)
+		{
+			if (RightSidedLimit != nextKey.RightSidedLimit)
+			{
+				OutSlope = float.PositiveInfinity;
+				// nextKey.InSlope is already 0
+			}
+			// else { this.OutSlope and nextKey.InSlope are already 0 }
+		}
+
+		private void CalculateSpecialSlopes(StreamedCurveKey nextKey)
+		{
+			bool isDiscontinuous = HasDifferentLimits();
 			if (RightSidedLimit == nextKey.RightSidedLimit)
 			{
-				OutSlope = 0f;
-				nextKey.InSlope = 0f;
-				nextKey.LeftSidedLimit = RightSidedLimit;
+				// OutSlope IS Zero or -Infinity
+				// but -Inf can also recreate Zero curves, so that will be used
+				SetNegInfSlope(nextKey, isDiscontinuous);
 				return;
 			}
+
+			if (isDiscontinuous) // * this estimation can be source of error
+			{
+				// OutSlope SHOULD be -Infinity
+				SetNegInfSlope(nextKey, true);
+				return;
+			}
+
+			// OutSlope SHOULD be Zero or +Infinity
+			// but +Inf can also recreate Zero curves, so that will be used
+			SetPosInfSlope();
+		}
+
+		private void SetNegInfSlope(StreamedCurveKey nextKey, bool isDiscontinuous)
+		{
+			OutSlope = float.NegativeInfinity;
+			// nextKey.InSlope is already 0
+
+			// set next LSL only if next slope calculation needs it
+			if (nextKey.Coefficient == default)
+			{
+				nextKey.LeftSidedLimit = RightSidedLimit;
+			}
+
+			// fix Value for true -Infinity slopes
+			if (isDiscontinuous)
+			{
+				RightSidedLimit = (float)LeftSidedLimit;
+			}
+		}
+
+		private void SetPosInfSlope()
+		{
 			OutSlope = float.PositiveInfinity;
-			nextKey.InSlope = 0f;
-			// don't do nextKey.LeftSidedLimit=RightSidedLimit here, because having 2 consecutive keys
-			// with outSlope +Inf and -Inf is illegal (Editor corrects it)
+			// nextKey.InSlope is already 0
+
+			/// Don't modify nextKey.LeftSidedLimit here!
+			/// If nextKey.LeftSidedLimit is kept equal to nextKey.RightSidedLimit,
+			/// then nextKey can't produce OutSlope of -Infinity
+			/// +Inf outslope followed by -Inf outslope is an "illegal configuration".
+		}
+
+		private bool HasDifferentLimits()
+		{
+			if (RightSidedLimit != LeftSidedLimit)
+			{
+				if (ExactLeftSidedLimit)
+				{
+					return true;
+				}
+
+				// check percentage difference. this estimation can be source of error
+				double diff = double.Abs((double)RightSidedLimit - LeftSidedLimit);
+				double div = double.Max(double.Abs(LeftSidedLimit), float.Abs(RightSidedLimit));
+				const float ROUNDING_ERROR = 5e-7f; // arbitrary small value
+				/// normally the difference between Right and Left Sided Limits
+				/// should be "big"/much greater than a rounding error.
+				if (diff / div > ROUNDING_ERROR)
+				{
+					return true;
+				}
+			}
+			return false;
 		}
 
 		/// <summary>
@@ -100,7 +174,15 @@ namespace AssetRipper.Processing.AnimationClips.Editor
 		/// outSlope=-Infinity creates a discontinuity on the curve,
 		/// making the CurveKey value differ from its right side.
 		/// </remarks>
-		private float LeftSidedLimit { get; set; }
+		private double LeftSidedLimit { get; set; }
+		/// <summary>
+		/// Used only to calculate slopes.
+		/// </summary>
+		/// <remarks>
+		/// Should be <c>false</c> if <c>LeftSidedLimit</c> was set from an arithmetic calculation
+		/// instead of from <c>Read()</c>, meaning its not 100% reliable for equality comparison.
+		/// </remarks>
+		private bool ExactLeftSidedLimit { get; set; } = true;
 		public float InSlope { get; private set; }
 		public float OutSlope { get; private set; }
 	}
