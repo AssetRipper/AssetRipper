3a73f405f5a9e2fd5e1b81b9bba6d987e4dbd3fb
ds5678 <49847914+ds5678@users.noreply.github.com>
2024-06-01 11:35:21 -0700

    Move OriginalPathDetails to a separate file

--- Source/AssetRipper.Assets/UnityObjectBase.cs
+++ Source/AssetRipper.Assets/UnityObjectBase.cs
@@ -7,7 +7,7 @@ namespace AssetRipper.Assets;
 /// <summary>
 /// The artificial base class for all generated Unity classes which inherit from Object.
 /// </summary>
-public abstract class UnityObjectBase : UnityAssetBase, IUnityObjectBase
+public abstract partial class UnityObjectBase : UnityAssetBase, IUnityObjectBase
 {
 	protected UnityObjectBase(AssetInfo assetInfo)
 	{
@@ -107,77 +107,4 @@ public abstract class UnityObjectBase : UnityAssetBase, IUnityObjectBase
 	/// The name of the asset bundle containing this asset.
 	/// </summary>
 	public string? AssetBundleName { get; set; }
-
-	private sealed class OriginalPathDetails
-	{
-		private string? directory;
-		private string? name;
-		private string? extension;
-		private string? fullPath;
-
-		public string? Directory
-		{
-			get => directory;
-			set
-			{
-				directory = value;
-				fullPath = CalculatePath();
-			}
-		}
-
-		public string? Name
-		{
-			get => name;
-			set
-			{
-				name = value;
-				fullPath = CalculatePath();
-			}
-		}
-
-		/// <summary>
-		/// Not including the period
-		/// </summary>
-		public string? Extension
-		{
-			get => extension;
-			set
-			{
-				extension = RemovePeriod(value);
-				fullPath = CalculatePath();
-			}
-		}
-
-		public string? FullPath
-		{
-			get => fullPath;
-			set
-			{
-				if (value != fullPath)
-				{
-					fullPath = value;
-					Directory = Path.GetDirectoryName(value);
-					Name = Path.GetFileNameWithoutExtension(value);
-					Extension = RemovePeriod(Path.GetExtension(value));
-				}
-			}
-		}
-
-		private string NameWithExtension => string.IsNullOrEmpty(Extension) ? Name ?? "" : $"{Name}.{Extension}";
-
-		public override string? ToString() => FullPath;
-
-		private string CalculatePath()
-		{
-			return Directory is null
-				? NameWithExtension
-				: Path.Combine(Directory, NameWithExtension);
-		}
-
-		[return: NotNullIfNotNull(nameof(str))]
-		private static string? RemovePeriod(string? str)
-		{
-			return string.IsNullOrEmpty(str) || str[0] != '.' ? str : str.Substring(1);
-		}
-	}
 }
