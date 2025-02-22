a59cf55442f3fa403a3cc86e665cc6352e4fad86
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-02-22 10:51:01 -0800

    Treat UnityEngine.UI.dll as a nonreference assembly * Also use generated regexes

--- Source/AssetRipper.Export.UnityProjects/Scripts/ReferenceAssemblies.cs
+++ Source/AssetRipper.Export.UnityProjects/Scripts/ReferenceAssemblies.cs
@@ -1,65 +1,74 @@
 ﻿using System.Text.RegularExpressions;
 
-namespace AssetRipper.Export.UnityProjects.Scripts
+namespace AssetRipper.Export.UnityProjects.Scripts;
+
+public static partial class ReferenceAssemblies
 {
-	public static class ReferenceAssemblies
+	[GeneratedRegex(@"^Unity(\.[0-9a-zA-Z]+)*(\.dll)?$")]
+	private static partial Regex UnityRegex { get; }
+
+	[GeneratedRegex(@"^UnityEngine(\.[0-9a-zA-Z]+)*(\.dll)?$")]
+	private static partial Regex UnityEngineRegex { get; }
+
+	[GeneratedRegex(@"^UnityEditor(\.[0-9a-zA-Z]+)*(\.dll)?$")]
+	private static partial Regex UnityEditorRegex { get; }
+
+	[GeneratedRegex(@"^System(\.[0-9a-zA-Z]+)*(\.dll)?$")]
+	private static partial Regex SystemRegex { get; }
+
+	private static HashSet<string> WhitelistAssemblies { get; } =
+	[
+		"UnityEngine.UI.dll"
+	];
+
+	private static HashSet<string> BlacklistAssemblies { get; } =
+	[
+		"mscorlib.dll",
+		"mscorlib",
+		"netstandard.dll",
+		"netstandard",
+		"Mono.Security.dll",
+		"Mono.Security"
+	];
+
+	private static HashSet<string> PredefinedAssemblies { get; } =
+	[
+		"Assembly-CSharp.dll",
+		"Assembly-CSharp",
+		"Assembly-CSharp-firstpass.dll",
+		"Assembly-CSharp-firstpass",
+		"Assembly-UnityScript.dll",
+		"Assembly-UnityScript",
+		"Assembly-UnityScript-firstpass.dll",
+		"Assembly-UnityScript-firstpass"
+	];
+
+	public static bool IsPredefinedAssembly(string assemblyName)
 	{
-		private static readonly Regex unityRegex = new Regex(@"^Unity(\.[0-9a-zA-Z]+)*(\.dll)?$", RegexOptions.Compiled);
-		private static readonly Regex unityEngineRegex = new Regex(@"^UnityEngine(\.[0-9a-zA-Z]+)*(\.dll)?$", RegexOptions.Compiled);
-		private static readonly Regex unityEditorRegex = new Regex(@"^UnityEditor(\.[0-9a-zA-Z]+)*(\.dll)?$", RegexOptions.Compiled);
-		private static readonly Regex systemRegex = new Regex(@"^System(\.[0-9a-zA-Z]+)*(\.dll)?$", RegexOptions.Compiled);
-		private static readonly HashSet<string> whitelistAssemblies = new HashSet<string>()
-		{
-
-		};
-		private static readonly HashSet<string> blacklistAssemblies = new HashSet<string>()
-		{
-			"mscorlib.dll",
-			"mscorlib",
-			"netstandard.dll",
-			"netstandard",
-			"Mono.Security.dll",
-			"Mono.Security"
-		};
-		private static readonly HashSet<string> predefinedAssemblies = new()
-		{
-			"Assembly-CSharp.dll",
-			"Assembly-CSharp",
-			"Assembly-CSharp-firstpass.dll",
-			"Assembly-CSharp-firstpass",
-			"Assembly-UnityScript.dll",
-			"Assembly-UnityScript",
-			"Assembly-UnityScript-firstpass.dll",
-			"Assembly-UnityScript-firstpass"
-		};
-
-		public static bool IsPredefinedAssembly(string assemblyName)
-		{
-			ArgumentNullException.ThrowIfNull(assemblyName);
-			return predefinedAssemblies.Contains(assemblyName);
-		}
-
-		public static bool IsReferenceAssembly(string assemblyName)
-		{
-			ArgumentNullException.ThrowIfNull(assemblyName);
-
-			if (IsWhiteListAssembly(assemblyName))
-			{
-				return false;
-			}
-
-			return IsBlackListAssembly(assemblyName)
-				|| IsUnityEngineAssembly(assemblyName)
-				//|| IsUnityAssembly(assemblyName)
-				|| IsSystemAssembly(assemblyName)
-				|| IsUnityEditorAssembly(assemblyName);
-		}
-
-		private static bool IsUnityAssembly(string assemblyName) => unityRegex.IsMatch(assemblyName);
-		public static bool IsUnityEngineAssembly(string assemblyName) => unityEngineRegex.IsMatch(assemblyName);
-		private static bool IsUnityEditorAssembly(string assemblyName) => unityEditorRegex.IsMatch(assemblyName);
-		private static bool IsSystemAssembly(string assemblyName) => systemRegex.IsMatch(assemblyName);
-		private static bool IsWhiteListAssembly(string assemblyName) => whitelistAssemblies.Contains(assemblyName);
-		private static bool IsBlackListAssembly(string assemblyName) => blacklistAssemblies.Contains(assemblyName);
+		ArgumentNullException.ThrowIfNull(assemblyName);
+		return PredefinedAssemblies.Contains(assemblyName);
 	}
+
+	public static bool IsReferenceAssembly(string assemblyName)
+	{
+		ArgumentNullException.ThrowIfNull(assemblyName);
+
+		if (IsWhiteListAssembly(assemblyName))
+		{
+			return false;
+		}
+
+		return IsBlackListAssembly(assemblyName)
+			|| IsUnityEngineAssembly(assemblyName)
+			//|| IsUnityAssembly(assemblyName)
+			|| IsSystemAssembly(assemblyName)
+			|| IsUnityEditorAssembly(assemblyName);
+	}
+
+	private static bool IsUnityAssembly(string assemblyName) => UnityRegex.IsMatch(assemblyName);
+	public static bool IsUnityEngineAssembly(string assemblyName) => UnityEngineRegex.IsMatch(assemblyName);
+	private static bool IsUnityEditorAssembly(string assemblyName) => UnityEditorRegex.IsMatch(assemblyName);
+	private static bool IsSystemAssembly(string assemblyName) => SystemRegex.IsMatch(assemblyName);
+	private static bool IsWhiteListAssembly(string assemblyName) => WhitelistAssemblies.Contains(assemblyName);
+	private static bool IsBlackListAssembly(string assemblyName) => BlacklistAssemblies.Contains(assemblyName);
 }
