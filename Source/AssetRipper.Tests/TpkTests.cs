8ed547204c85d877d39a15e4858b2a6b04091aa3
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-02-24 17:05:40 -0800

    Add TpkTests

---
+++ Source/AssetRipper.Tests/TpkTests.cs
@@ -0,0 +1,25 @@
+﻿using AssetRipper.SourceGenerated;
+using AssetRipper.Tpk;
+using AssetRipper.Tpk.EngineAssets;
+using AssetRipper.Tpk.TypeTrees;
+
+namespace AssetRipper.Tests;
+
+internal class TpkTests
+{
+	[Test]
+	public void EngineAssetsTpkSuccessfullyExtracts()
+	{
+		TpkFile tpk = TpkFile.FromStream(EngineAssetsTpk.GetStream());
+		TpkEngineAssetsBlob blob = (TpkEngineAssetsBlob)tpk.GetDataBlob();
+		Assert.That(blob.Data, Is.Not.Empty);
+	}
+
+	[Test]
+	public void TypeTreeTpkSuccessfullyExtracts()
+	{
+		TpkFile tpk = TpkFile.FromStream(SourceTpk.GetStream());
+		TpkTypeTreeBlob blob = (TpkTypeTreeBlob)tpk.GetDataBlob();
+		Assert.That(blob.ClassInformation, Is.Not.Empty);
+	}
+}
