73ea2b6c7cfeb6d5ac36814202ded82a795011c2
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-03-23 20:34:12 -0700

    Fix IsUnmanagedAttribute injection

--- Source/AssetRipper.Processing/Assemblies/AttributePolyfill.cs
+++ Source/AssetRipper.Processing/Assemblies/AttributePolyfill.cs
@@ -256,11 +256,15 @@ internal readonly record struct AttributePolyfill(string Namespace, string Name,
 		""";
 	public static AttributePolyfill TypeForwardedToAttribute => new("System.Runtime.CompilerServices", "TypeForwardedToAttribute", TypeForwardedToAttributeCode);
 
+	/// <summary>
+	/// Modified version of the IsUnmanagedAttribute from the .NET runtime.
+	/// References to System.ComponentModel have been removed because they're in System.dll instead of mscorlib.
+	/// </summary>
 	private const string IsUnmanagedAttributeCode = """
 		// Licensed to the .NET Foundation under one or more agreements.
 		// The .NET Foundation licenses this file to you under the MIT license.
 
-		using System.ComponentModel;
+		//using System.ComponentModel;
 
 		namespace System.Runtime.CompilerServices
 		{
@@ -268,7 +272,7 @@ internal readonly record struct AttributePolyfill(string Namespace, string Name,
 			/// Reserved for use by a compiler for tracking metadata.
 			/// This attribute should not be used by developers in source code.
 			/// </summary>
-			[EditorBrowsable(EditorBrowsableState.Never)]
+			//[EditorBrowsable(EditorBrowsableState.Never)]
 			[AttributeUsage(AttributeTargets.All)]
 			public sealed class IsUnmanagedAttribute : Attribute
 			{
