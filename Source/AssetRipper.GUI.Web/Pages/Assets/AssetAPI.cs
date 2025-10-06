58d0496f3c1c98e59e4046195709d635a549b63e
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-10-05 20:12:53 -0700

    Save button for audio assets
    Resolves #1950

--- Source/AssetRipper.GUI.Web/Pages/Assets/AssetAPI.cs
+++ Source/AssetRipper.GUI.Web/Pages/Assets/AssetAPI.cs
@@ -32,7 +32,6 @@ using AssetRipper.Yaml;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using SharpGLTF.Scenes;
-using SharpGLTF.Schema2;
 using System.Globalization;
 using System.Runtime.InteropServices;
 
@@ -165,7 +164,7 @@ internal static class AssetAPI
 	#region Audio
 	public static string GetAudioUrl(AssetPath path, string? extension = null)
 	{
-		return $"{Urls.Audio}?{GetPathQuery(path)}";
+		return $"{Urls.Audio}?{GetPathQuery(path)}{GetExtensionQuerySuffix(extension)}";
 	}
 
 	public static Task GetAudioData(HttpContext context)
@@ -182,15 +181,19 @@ internal static class AssetAPI
 		}
 		else if (AudioClipDecoder.TryDecode(clip, out byte[]? decodedAudioData, out string? extension, out _))
 		{
-			if (extension is "ogg")
+			if (context.Request.Query.TryGetValue(Extension, out string? desiredExtension))
 			{
-				byte[] wavData = AudioConverter.OggToWav(decodedAudioData);
-				if (wavData.Length > 0)
+				if (extension is "ogg" && desiredExtension is "wav")
 				{
-					decodedAudioData = wavData;
-					extension = "wav";
+					byte[] wavData = AudioConverter.OggToWav(decodedAudioData);
+					if (wavData.Length > 0)
+					{
+						decodedAudioData = wavData;
+						extension = "wav";
+					}
 				}
 			}
+
 			return Results.Bytes(decodedAudioData, $"audio/{extension}").ExecuteAsync(context);
 		}
 		else
