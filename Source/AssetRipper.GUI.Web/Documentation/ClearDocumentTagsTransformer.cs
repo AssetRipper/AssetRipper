a09c8160408b25053f87a4d8cfdfe3d92686e7bd
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-11-19 21:08:17 -0800

    Update to .NET 10

--- Source/AssetRipper.GUI.Web/Documentation/ClearDocumentTagsTransformer.cs
+++ Source/AssetRipper.GUI.Web/Documentation/ClearDocumentTagsTransformer.cs
@@ -1,5 +1,5 @@
 ﻿using Microsoft.AspNetCore.OpenApi;
-using Microsoft.OpenApi.Models;
+using Microsoft.OpenApi;
 
 namespace AssetRipper.GUI.Web.Documentation;
 
--- Source/AssetRipper.Numerics/VectorExtensions.cs
+++ Source/AssetRipper.Numerics/VectorExtensions.cs
@@ -1,19 +1,7 @@
-﻿using System.Runtime.CompilerServices;
-
-namespace AssetRipper.Numerics;
+﻿namespace AssetRipper.Numerics;
 
 public static class VectorExtensions
 {
-	public static Vector2 AsVector2(this Vector3 vector3)
-	{
-		return Unsafe.As<Vector3, Vector2>(ref vector3);
-	}
-
-	public static Vector3 AsVector3(this Vector2 vector2)
-	{
-		return new Vector3(vector2, 0);
-	}
-
 	public static Vector3 InvertX(this Vector3 vector)
 	{
 		return new Vector3(-vector.X, vector.Y, vector.Z);
