afeaabae537ff124b3e652e4d34d2d8dad4b7420
Olsc <918736610@qq.com>
2025-12-09 11:08:04 +0800

    Fix texture-sprite filtering

--- Source/AssetRipper.Export.UnityProjects/Textures/TextureExportCollection.cs
+++ Source/AssetRipper.Export.UnityProjects/Textures/TextureExportCollection.cs
@@ -24,14 +24,14 @@ public class TextureExportCollection : AssetsExportCollection<ITexture2D>
 
 		if (exportSprites && spriteInformationObject.Sprites.Count > 0)
 		{
-			foreach ((ISprite? sprite, ISpriteAtlas? _) in spriteInformationObject.Sprites)
-			{
-				Debug.Assert(sprite.TryGetTexture() == Asset);
-				if (sprite.TryGetTexture() == Asset)
-				{
-					AddAsset(sprite);
-				}
-			}
+            foreach ((ISprite? sprite, ISpriteAtlas? _) in spriteInformationObject.Sprites)
+            {
+                Debug.Assert(sprite.TryGetTexture()?.AssetInfo == Asset.AssetInfo);
+                if (sprite.TryGetTexture()?.AssetInfo == Asset.AssetInfo)
+                {
+                    AddAsset(sprite);
+                }
+            }
 		}
 		AddAsset(spriteInformationObject);
 	}
@@ -42,7 +42,7 @@ public class TextureExportCollection : AssetsExportCollection<ITexture2D>
 		if (m_convert)
 		{
 			ITextureImporter importer = ImporterFactory.GenerateTextureImporter(container, texture);
-			AddSprites(container, importer, GetFilteredSpriteInfo());
+            AddSprites(container, importer, GetFilteredSpriteInfo());
 			return importer;
 		}
 		else
@@ -140,23 +140,24 @@ public class TextureExportCollection : AssetsExportCollection<ITexture2D>
 		}
 	}
 
-	private IReadOnlyDictionary<ISprite, ISpriteAtlas?>? GetFilteredSpriteInfo()
-	{
-		SpriteInformationObject? group = (SpriteInformationObject?)Asset.MainAsset;
-		if (group is null)
-		{
-			return null;
-		}
-		var dict = new Dictionary<ISprite, ISpriteAtlas?>();
-		foreach (var kvp in group.Sprites)
-		{
-			if (kvp.Key.TryGetTexture() == Asset)
-			{
-				dict[kvp.Key] = kvp.Value;
-			}
-		}
-		return dict;
-	}
+    private IReadOnlyDictionary<ISprite, ISpriteAtlas?>? GetFilteredSpriteInfo()
+    {
+        SpriteInformationObject? group = (SpriteInformationObject?)Asset.MainAsset;
+        if (group is null)
+        {
+            return null;
+        }
+        var dict = new Dictionary<ISprite, ISpriteAtlas?>();
+        foreach (var kvp in group.Sprites)
+        {
+            var tex = kvp.Key.TryGetTexture();
+            if (tex is not null && tex.AssetInfo == Asset.AssetInfo)
+            {
+                dict[kvp.Key] = kvp.Value;
+            }
+        }
+        return dict.Count > 0 ? dict : group.Sprites;
+    }
 
 	private static void AddSpriteSheet(ITextureImporter importer, IReadOnlyDictionary<ISprite, ISpriteAtlas?> textureSpriteInformation)
 	{
