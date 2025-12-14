b4c34bb1d488e09527a30c914c860c4f57df2b58
ds5678 <49847914+ds5678@users.noreply.github.com>
2025-12-14 15:00:09 -0800

    Fix cropping for swizzled switch textures
    Resolves #2044

--- Source/AssetRipper.SourceGenerated.Extensions/SwitchSwizzle.cs
+++ Source/AssetRipper.SourceGenerated.Extensions/SwitchSwizzle.cs
@@ -27,6 +27,12 @@ public static class SwitchSwizzle
 		0, 1, 0, 1, 2, 3, 2, 3, 4, 5, 4, 5, 6, 7, 6, 7, 0, 1, 0, 1, 2, 3, 2, 3, 4, 5, 4, 5, 6, 7, 6, 7
 	];
 
+	/// <summary>
+	/// Calculates the smallest integer greater than or equal to the result of dividing two integers.
+	/// </summary>
+	/// <param name="a">The dividend to be divided.</param>
+	/// <param name="b">The divisor by which to divide. Must not be zero.</param>
+	/// <returns>The smallest integer greater than or equal to <paramref name="a"/> divided by <paramref name="b"/>.</returns>
 	private static int CeilDivide(int a, int b)
 	{
 		return (a + b - 1) / b;
@@ -42,38 +48,47 @@ public static class SwitchSwizzle
 		TextureFormat realFormat = GetCorrectedSwitchTextureFormat(texture.Format_C28E);
 
 		// Format is unsupported, we back out
-		if (GetTextureFormatBlockSize(realFormat) is not { } blockSize)
+		if (GetTexelDimensions(realFormat) is not { } texelDimensions)
 		{
 			return data;
 		}
 
+		int blockSize = GetBlockSize(realFormat);
+
+		Debug.Assert(TexelByteSize % blockSize == 0, "Texel size should be a multiple of block size.");
+		int blocksPerTexel = TexelByteSize / blockSize;
+
+		Debug.Assert(blocksPerTexel > 0);
+		Debug.Assert(texelDimensions.Width % blocksPerTexel == 0, "Texel width should be a multiple of blocks per texel.");
+		Size blockDimensions = new(texelDimensions.Width / blocksPerTexel, texelDimensions.Height);
+
 		byte[] newData = new byte[data.Length];
 
 		Debug.Assert(texture.PlatformBlob_C28 is not null && texture.PlatformBlob_C28.Length >= 12);
-		int blockHeight = GetBlockHeightByPlatformBlob(texture.PlatformBlob_C28, texture.Collection.EndianType);
+		int texelHeight = GetTexelHeightByPlatformBlob(texture.PlatformBlob_C28, texture.Collection.EndianType);
 
-		Size paddedSize = GetPaddedTextureSize(texture.Width_C28, texture.Height_C28, blockSize.Width, blockSize.Height, blockHeight);
+		Size paddedDimensions = GetPaddedTextureDimensions(texture.Width_C28, texture.Height_C28, texelDimensions.Width, texelDimensions.Height, texelHeight);
 
-		int blockCountX = CeilDivide(paddedSize.Width, blockSize.Width);
-		int blockCountY = CeilDivide(paddedSize.Height, blockSize.Height);
+		int texelCountX = CeilDivide(paddedDimensions.Width, texelDimensions.Width);
+		int texelCountY = CeilDivide(paddedDimensions.Height, texelDimensions.Height);
 
-		int gobCountX = blockCountX / GobXTexelCount;
-		int gobCountY = blockCountY / GobYTexelCount;
+		int gobCountX = texelCountX / GobXTexelCount;
+		int gobCountY = texelCountY / GobYTexelCount;
 
 		int srcPos = 0;
-		for (int i = 0; i < gobCountY / blockHeight; i++)
+		for (int i = 0; i < gobCountY / texelHeight; i++)
 		{
 			for (int j = 0; j < gobCountX; j++)
 			{
-				for (int k = 0; k < blockHeight; k++)
+				for (int k = 0; k < texelHeight; k++)
 				{
 					for (int l = 0; l < BlocksInGob; l++)
 					{
 						int gobX = GobXPoses[l];
 						int gobY = GobYPoses[l];
 						int gobDstX = j * GobXTexelCount + gobX;
-						int gobDstY = (i * blockHeight + k) * GobYTexelCount + gobY;
-						int gobDstLinPos = gobDstY * blockCountX * TexelByteSize + gobDstX * TexelByteSize;
+						int gobDstY = (i * texelHeight + k) * GobYTexelCount + gobY;
+						int gobDstLinPos = gobDstY * texelCountX * TexelByteSize + gobDstX * TexelByteSize;
 
 						data.AsSpan(srcPos, TexelByteSize).CopyTo(newData.AsSpan(gobDstLinPos, TexelByteSize));
 
@@ -87,64 +102,49 @@ public static class SwitchSwizzle
 		// The resulting arrays will have extra unused data at the end, but we need to leave it there
 		// because TextureConverter expects the data to be at least texture.CompleteImageSize bytes long.
 
-		if (blockSize.Height > 1)
+		byte[] result = newData;
+
+		int srcBlockCountX = texelCountX * blocksPerTexel;
+		int dstBlockCountX = CeilDivide(texture.Width_C28, blockDimensions.Width);
+		if (dstBlockCountX != srcBlockCountX)
 		{
-			int dstBlockCountX = CeilDivide(texture.Width_C28, blockSize.Width);
-			if (dstBlockCountX != blockCountX)
+			byte[] croppedData = new byte[newData.Length];
+			int dstBlockCountY = CeilDivide(texture.Height_C28, blockDimensions.Height);
+			for (int y = 0; y < dstBlockCountY; y++)
 			{
-				byte[] croppedImageData = new byte[newData.Length];
-				int dstBlockCountY = CeilDivide(texture.Height_C28, blockSize.Height);
-				for (int y = 0; y < dstBlockCountY; y++)
+				newData.AsSpan(y * srcBlockCountX * blockSize, dstBlockCountX * blockSize).CopyTo(croppedData.AsSpan(y * dstBlockCountX * blockSize));
+			}
+
+			result = croppedData;
+		}
+
+		if (realFormat != texture.Format_C28E)
+		{
+			// Convert RGBA to RGB
+			Debug.Assert(blockSize % 4 == 0);
+			int actualPixelSize = blockSize / 4 * 3;
+			byte[] finalData = new byte[result.Length];
+			for (int y = 0; y < texture.Height_C28; y++)
+			{
+				int srcOffset = y * texture.Width_C28 * blockSize;
+				int dstOffset = y * texture.Width_C28 * actualPixelSize;
+				for (int x = 0; x < texture.Width_C28; x++)
 				{
-					newData.AsSpan(y * blockCountX * TexelByteSize, dstBlockCountX * TexelByteSize).CopyTo(croppedImageData.AsSpan(y * dstBlockCountX * TexelByteSize));
+					int srcIndex = srcOffset + x * blockSize;
+					int dstIndex = dstOffset + x * actualPixelSize;
+					result.AsSpan(srcIndex, actualPixelSize).CopyTo(finalData.AsSpan(dstIndex, actualPixelSize));
 				}
-				return croppedImageData;
 			}
-
-			return newData;
+			result = finalData;
 		}
 
-		int pixelSize = TexelByteSize / (blockSize.Width * blockSize.Height);
-		if (newData.Length <= texture.Width_C28 * texture.Height_C28 * pixelSize)
-		{
-			return newData;
-		}
-
-		byte[] croppedData = new byte[newData.Length];
-		int rowBytesSrc = paddedSize.Width * pixelSize;
-		int rowBytesDst = texture.Width_C28 * pixelSize;
-		for (int y = 0; y < texture.Height_C28; y++)
-		{
-			newData.AsSpan(y * rowBytesSrc, rowBytesDst).CopyTo(croppedData.AsSpan(y * rowBytesDst, rowBytesDst));
-		}
-
-		if (realFormat == texture.Format_C28E)
-		{
-			return croppedData;
-		}
-
-		// Convert RGBA to RGB
-		Debug.Assert(pixelSize % 4 == 0);
-		int actualPixelSize = pixelSize / 4 * 3;
-		byte[] finalData = new byte[croppedData.Length];
-		for (int y = 0; y < texture.Height_C28; y++)
-		{
-			int srcOffset = y * texture.Width_C28 * pixelSize;
-			int dstOffset = y * texture.Width_C28 * actualPixelSize;
-			for (int x = 0; x < texture.Width_C28; x++)
-			{
-				int srcIndex = srcOffset + x * pixelSize;
-				int dstIndex = dstOffset + x * actualPixelSize;
-				croppedData.AsSpan(srcIndex, actualPixelSize).CopyTo(finalData.AsSpan(dstIndex, actualPixelSize));
-			}
-		}
-		return finalData;
+		return result;
 	}
 
 	/// <remarks>
-	/// Some of these are guesses based on others and that texels are always 16 bytes in size.
+	/// Some of these are educated guesses. Guesses are based on known values for others and the fact that texels are always 16 bytes in size.
 	/// </remarks>
-	private static Size? GetTextureFormatBlockSize(TextureFormat textureFormat) => textureFormat switch
+	private static Size? GetTexelDimensions(TextureFormat textureFormat) => textureFormat switch
 	{
 		TextureFormat.Alpha8 => new Size(16, 1),
 		TextureFormat.ARGB4444 => new Size(8, 1),
@@ -177,14 +177,47 @@ public static class SwitchSwizzle
 		_ => null,
 	};
 
-	private static Size GetPaddedTextureSize(int width, int height, int blockWidth, int blockHeight, int gobsPerBlock)
+	private static int GetBlockSize(TextureFormat textureFormat) => textureFormat switch
+	{
+		TextureFormat.Alpha8 => 1,
+		TextureFormat.ARGB4444 => 1,
+		TextureFormat.RGBA4444 => 1,
+		TextureFormat.ARGB32 => 4,
+		TextureFormat.BGRA32_14 or TextureFormat.BGRA32_37 => 4,
+		TextureFormat.RGB565 => 1,
+		TextureFormat.DXT1 => 8,
+		TextureFormat.DXT5 => 16,
+		TextureFormat.BC4 => 8,
+		TextureFormat.BC5 => 16,
+		TextureFormat.BC6H => 16,
+		TextureFormat.BC7 => 16,
+		TextureFormat.ASTC_RGB_4x4 or TextureFormat.ASTC_RGBA_4x4 or TextureFormat.ASTC_HDR_4x4 => 16,
+		TextureFormat.ASTC_RGB_5x5 or TextureFormat.ASTC_RGBA_5x5 or TextureFormat.ASTC_HDR_5x5 => 16,
+		TextureFormat.ASTC_RGB_6x6 or TextureFormat.ASTC_RGBA_6x6 or TextureFormat.ASTC_HDR_6x6 => 16,
+		TextureFormat.ASTC_RGB_8x8 or TextureFormat.ASTC_RGBA_8x8 or TextureFormat.ASTC_HDR_8x8 => 16,
+		TextureFormat.ASTC_RGB_10x10 or TextureFormat.ASTC_RGBA_10x10 or TextureFormat.ASTC_HDR_10x10 => 16,
+		TextureFormat.ASTC_RGB_12x12 or TextureFormat.ASTC_RGBA_12x12 or TextureFormat.ASTC_HDR_12x12 => 16,
+		TextureFormat.R8 or TextureFormat.R8_SIGNED => 1,
+		TextureFormat.RG16 or TextureFormat.RG16_SIGNED => 2,
+		TextureFormat.RGBA32 or TextureFormat.RGBA32_SIGNED => 4,
+		TextureFormat.R16 or TextureFormat.R16_SIGNED or TextureFormat.RHalf => 2,
+		TextureFormat.RG32 or TextureFormat.RG32_SIGNED or TextureFormat.RGHalf => 4,
+		TextureFormat.RGBA64 or TextureFormat.RGBA64_SIGNED or TextureFormat.RGBAHalf => 8,
+		TextureFormat.RFloat => 4,
+		TextureFormat.RGFloat => 8,
+		TextureFormat.RGBAFloat => 16,
+		TextureFormat.ARGBFloat => 16,
+		_ => throw new NotSupportedException(),
+	};
+
+	private static Size GetPaddedTextureDimensions(int width, int height, int blockWidth, int blockHeight, int gobsPerBlock)
 	{
 		width = CeilDivide(width, blockWidth * GobXTexelCount) * blockWidth * GobXTexelCount;
 		height = CeilDivide(height, blockHeight * GobYTexelCount * gobsPerBlock) * blockHeight * GobYTexelCount * gobsPerBlock;
 		return new Size(width, height);
 	}
 
-	private static int GetBlockHeightByPlatformBlob(byte[] platformBlob, EndianType endianType)
+	private static int GetTexelHeightByPlatformBlob(byte[] platformBlob, EndianType endianType)
 	{
 		return 1 << new EndianSpanReader(platformBlob.AsSpan(8), endianType).ReadInt32();
 	}
