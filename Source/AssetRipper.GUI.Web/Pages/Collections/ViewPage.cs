73683ecbab98070829b26e750e194ce2f6c39b51
ssap0 <70007733+ssap0@users.noreply.github.com>
2026-02-13 10:39:15 +0800

    naive method of paging
    simple functions and UI of paging

--- Source/AssetRipper.GUI.Web/Pages/Collections/ViewPage.cs
+++ Source/AssetRipper.GUI.Web/Pages/Collections/ViewPage.cs
@@ -11,6 +11,8 @@ public sealed class ViewPage : DefaultPage
 
 	public override string GetTitle() => Collection.Name;
 
+	private readonly List<int> assetsPerPage = new() {500, 1000, 2000, 3000, 5000};// Max 5k per page?
+
 	public override void WriteInnerContent(TextWriter writer)
 	{
 		new H1(writer).Close(GetTitle());
@@ -31,6 +33,7 @@ public sealed class ViewPage : DefaultPage
 			var availableClasses = Collection.Select(a => a.ClassName).Distinct().Order().ToList();
 			if (availableClasses.Count > 1)
 			{
+				//i: set the filter items and UI? (since this doesn't change for a collection
 				new Label(writer).WithFor("classFilter").WithClass("me-2").Close(Localization.Class);
 
 				using (new Select(writer)
@@ -50,7 +53,9 @@ public sealed class ViewPage : DefaultPage
 					}
 				}
 			}
-			
+
+			CreatePageOption(writer, Collection.Count);
+				
 			using (new Table(writer).WithClass("table").End())
 			{
 				using (new Thead(writer).End())
@@ -124,6 +129,8 @@ public sealed class ViewPage : DefaultPage
 		// Add client-side filtering script
 		using (new Script(writer).End())
 		{
+			//Todo: add page selection filter
+			//Todo: add page reseting code after filtering
 			writer.Write("""
 				document.addEventListener('DOMContentLoaded', function() {
 					const classFilter = document.getElementById('classFilter');
@@ -131,7 +138,7 @@ public sealed class ViewPage : DefaultPage
 						classFilter.addEventListener('change', function() {
 							const selectedClass = this.value;
 							const rows = document.querySelectorAll('#assetsTable tr');
-							
+
 							rows.forEach(function(row) {
 								const rowClass = row.getAttribute('data-class');
 								if (selectedClass === '' || rowClass === selectedClass) {
@@ -142,8 +149,130 @@ public sealed class ViewPage : DefaultPage
 							});
 						});
 					}
+
+					const pageFilter = document.getElementById('assetPerPage');
+					if (pageFilter) {
+						pageFilter.addEventListener('change', function() {
+							const selectedCount = parseInt(this.value);
+							const pageNo = parseInt(document.getElementById('pageNo').innerHTML);
+							const rows = document.querySelectorAll('#assetsTable tr');
+							
+							const start = selectedCount * (pageNo - 1);
+							for(var i = 0; i < rows.length; ++i){
+								if(i >= start && i < selectedCount * pageNo){
+									rows[i].style.display = '';
+								}else {
+									rows[i].style.display = 'none';
+								}
+							}
+						});
+					}
+
+					const prevButton = document.getElementById('prevBtn');
+					const nextButton = document.getElementById('nextBtn');
+					if(prevButton){
+						prevButton.addEventListener('click', function() {
+							var pageNo = parseInt(document.getElementById('pageNo').innerHTML);
+							--pageNo;
+							document.getElementById('pageNo').innerHTML = "" + pageNo;
+
+							if(pageNo == 1){
+								this.disabled = true;
+							}
+
+							if(nextButton){
+								nextButton.disabled = false;
+							}
+
+							if (pageFilter) {
+								var event = new Event('change');
+								pageFilter.dispatchEvent(event);
+							}
+						});
+					}
+
+					if(nextButton){
+						nextButton.addEventListener('click', function() {
+							var pageNo = parseInt(document.getElementById('pageNo').innerHTML);
+							++pageNo;
+							document.getElementById('pageNo').innerHTML = "" + pageNo;
+				
+							if(prevButton){
+								prevButton.disabled = false;
+							}
+
+							if (pageFilter) {
+								const rows = document.querySelectorAll('#assetsTable tr');
+								const selectedCount = parseInt(pageFilter.value);
+								if(rows.length <= selectedCount * pageNo){
+									this.disabled = true;
+								}
+
+								var event = new Event('change');
+								pageFilter.dispatchEvent(event);
+							}
+						});
+					}
 				});
 				""");
+		}
+	}
+
+	private void CreatePageOption(TextWriter writer, int collectionCount)
+	{
+		//Todo: add page UI, add localization:
+		/// page
+		/// prev
+		/// next
+		/// Assets Per Page
+		/// Todo: Un Naive the method---add in 
+		/// Filters
+		/// Page updates on Asset Per page changes
+		new Label(writer).WithFor("pageCollection").WithClass("me-2").Close("Page");
+		new Button(writer)
+				.WithId("prevBtn")
+				.WithClass("ms-2")
+				.WithType("button")
+				.WithCustomAttribute("v-else")
+				.WithCustomAttribute("disabled")
+				.Close("prev");
+
+		new Label(writer).WithId("pageNo").WithClass("ms-2").Close("1");// dropdown also
+
+		Button b = new Button(writer)
+				.WithId("nextBtn")
+				.WithType("button")
+				.WithClass("ms-2")
+				.WithCustomAttribute("v-else");
+
+		if (Collection.Count <= assetsPerPage[0])
+		{
+			b.WithCustomAttribute("disabled");
+		}
+
+		b.Close("next");
+
+		new Label(writer).WithClass("ms-2").Close("Assets Per Page");
+
+		using (new Select(writer)
+			.WithId("assetPerPage")
+			.WithClass("ms-2")
+			.End())
+		{
+			foreach (int count in assetsPerPage)
+			{
+				new Option(writer)
+					.WithValue(string.Empty)
+					.Close(Localization.All);
+
+				if (collectionCount >= count)
+				{
+
+					new Option(writer)
+					.WithValue("" + count)
+					.Close("" + count);
+				}
+			}
 		}
 	}
 }
