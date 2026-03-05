a125df6af10cbb67f8336006a8599b8d79a5c730
Creeper1212 <148118420+Creeper1212@users.noreply.github.com>
2026-03-05 12:41:51 +0100

    Add files via upload

---
+++ Source/AssetRipper.Processing/GameObjectNameCleaner.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AssetRipper.Processing;
+
+public static partial class GameObjectNameCleaner
+{
+	[GeneratedRegex("\\([0-9]+\\)$", RegexOptions.Compiled)]
+	private static partial Regex CopySuffixRegex();
+
+	public static string CleanName(string name)
+	{
+		string noClones = name.Replace("(Clone)", "", StringComparison.Ordinal);
+		return CopySuffixRegex().Replace(noClones, "").Trim();
+	}
+}
