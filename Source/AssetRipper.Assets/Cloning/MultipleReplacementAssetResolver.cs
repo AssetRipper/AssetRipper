ad958732c09fc83686ddd85df3310f03ff5f9763
ds5678 <49847914+ds5678@users.noreply.github.com>
2026-07-11 17:54:49 -0700

    Cleanup: Add MultipleReplacementAssetResolver and refactor ExportAsset
    Introduce MultipleReplacementAssetResolver using FrozenDictionary for asset replacement resolution. Refactor ExportAsset in ExportCollectionBase to use 'directory' instead of 'path' for improved clarity.

---
+++ Source/AssetRipper.Assets/Cloning/MultipleReplacementAssetResolver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Frozen;
+
+namespace AssetRipper.Assets.Cloning;
+
+public class MultipleReplacementAssetResolver : IAssetResolver
+{
+	public FrozenDictionary<IUnityObjectBase, IUnityObjectBase> Replacements { get; }
+
+	public MultipleReplacementAssetResolver(IReadOnlyDictionary<IUnityObjectBase, IUnityObjectBase> replacements)
+	{
+		Replacements = replacements.ToFrozenDictionary();
+	}
+	public MultipleReplacementAssetResolver()
+	{
+		Replacements = FrozenDictionary<IUnityObjectBase, IUnityObjectBase>.Empty;
+	}
+
+	public T? Resolve<T>(IUnityObjectBase? asset) where T : IUnityObjectBase
+	{
+		if (asset != null && Replacements.TryGetValue(asset, out IUnityObjectBase? replacement))
+		{
+			return TryCast<T>(replacement);
+		}
+		return TryCast<T>(asset);
+	}
+
+	private static T? TryCast<T>(IUnityObjectBase? asset) where T : IUnityObjectBase
+	{
+		return asset is T t ? t : default;
+	}
+}
